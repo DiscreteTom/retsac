35d63b22368a3852a82eed66f801208e351d88e7
DiscreteTom <discrete_tom@outlook.com>
2022-05-22 01:02:48 +0800

    playground support comments

--- example/playground.ts
+++ example/playground.ts
@@ -3,7 +3,17 @@ import * as readline from "readline";
 
 let lexer = Lexer.define({
   number: /^[0-9]+(?:\.[0-9]+)?\b/,
-}).ignore(/^[ \n\r\t]+/, "blank");
+})
+  .ignore(/^[ \n\r\t]+/, "blank")
+  .ignore(/^\/\/[^\n]*/, "single line comments")
+  .ignore((buffer) => {
+    if (buffer.startsWith("/*")) {
+      let index = buffer.indexOf("*/", 2);
+      if (index == -1) return buffer.length;
+      else return index + 2;
+    }
+    return 0;
+  }, "multiline comments");
 
 var rl = readline.createInterface({
   input: process.stdin,
