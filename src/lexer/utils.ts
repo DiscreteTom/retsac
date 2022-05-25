e98946d31cc365799e32761ed44ad2261741e689
DiscreteTom <discrete_tom@outlook.com>
2022-05-25 20:06:59 +0800

    lexer utils will return action

--- src/lexer/utils.ts
+++ src/lexer/utils.ts
@@ -1,11 +1,7 @@
-import { SimpleActionExec } from "./action";
+import { Action } from "./action";
 
-export function from_to(
-  from: string,
-  to: string,
-  acceptEof: boolean
-): SimpleActionExec {
-  return (buffer) => {
+export function from_to(from: string, to: string, acceptEof: boolean): Action {
+  return Action.from((buffer) => {
     if (buffer.startsWith(from)) {
       let index = buffer.indexOf(to, from.length);
       if (index == -1)
@@ -18,9 +14,9 @@ export function from_to(
       else return index + to.length;
     }
     return 0;
-  };
+  });
 }
 
-export function exact(s: string): SimpleActionExec {
-  return (buffer) => (buffer.startsWith(s) ? s.length : 0);
+export function exact(s: string): Action {
+  return Action.from((buffer) => (buffer.startsWith(s) ? s.length : 0));
 }
