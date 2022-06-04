1fd71d9fd191f32496d4c3caadd42fb4b6993c22
DiscreteTom <discrete_tom@outlook.com>
2022-06-04 11:53:23 +0800

    update example/calculator

--- example/calculator.ts
+++ example/calculator.ts
@@ -1,68 +1,63 @@
 import { Lexer } from "../src/lexer/lexer";
 import { Parser } from "../src/parser/parser";
-import * as readline from "readline";
-import { ASTNode } from "../src/parser/ast";
 import { exact } from "../src/lexer/utils";
-import { SimpleReducer, valueReducer } from "../src/parser/simple";
+import { SimpleNodeReducer, valueReducer } from "../src/parser/simple";
 
-let parser = new Parser()
-  .setLexer(
-    new Lexer()
-      .ignore(/^\s/)
-      .define({
-        number: /^[0-9]+(?:\.[0-9]+)?/,
-      })
-      .anonymous(exact(..."+-*/()"))
-  )
-  .addRule(
-    new SimpleReducer()
-      .define(
-        { exp: "number" },
-        ({ data, matched }) => (data.value = Number(matched[0].text))
-      )
-      .define({ exp: `'-' exp` }, (context) => {
-        // if previous node is an exp, the `- exp` should be `exp - exp`, reject
-        if (context.before.at(-1)?.type == "exp") return "reject";
-        // else, no previous, or previous is not exp
-        context.data.value = -context.matched[1].data.value;
-      })
-      .define(
-        { exp: `'(' exp ')'` },
-        valueReducer((values) => values[1])
-      )
-      .define({ exp: `exp '+' exp` }, ({ after, data, matched }) => {
-        if (after[0]?.text == "*" || after[0]?.text == "/") return "reject";
-        data.value = matched[0].data.value + matched[2].data.value;
-      })
-      .define({ exp: `exp '-' exp` }, ({ after, data, matched }) => {
-        if (after[0]?.text == "*" || after[0]?.text == "/") return "reject";
-        data.value = matched[0].data.value - matched[2].data.value;
-      })
-      .define(
-        { exp: `exp '*' exp` },
-        valueReducer((values) => values[0] * values[2])
-      )
-      .define(
-        { exp: `exp '/' exp` },
-        valueReducer((values) => values[0] / values[2])
-      )
-      .compile()
-  );
+let lexer = new Lexer()
+  .ignore(/^\s/)
+  .define({
+    number: /^[0-9]+(?:\.[0-9]+)?/,
+  })
+  .anonymous(exact(..."+-*/()"));
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false,
-  prompt: ">>> ",
-});
+let parser = new Parser(lexer).addNodeReducer(
+  new SimpleNodeReducer()
+    .define(
+      { exp: "number" },
+      valueReducer((_, { matched }) => Number(matched[0].text))
+    )
+    .define(
+      { exp: `'-' exp` },
+      valueReducer((values) => -values[1]),
+      // if previous node is an exp, the `- exp` should be `exp - exp`, reject
+      ({ before }) => before.at(-1)?.type == "exp"
+    )
+    .define(
+      { exp: `'(' exp ')'` },
+      valueReducer((values) => values[1])
+    )
+    .define(
+      { exp: `exp '+' exp | exp '-' exp` },
+      valueReducer((values, { matched }) =>
+        matched[1].text == "+" ? values[0] + values[2] : values[0] - values[2]
+      ),
+      ({ after }) => after[0]?.text == "*" || after[0]?.text == "/"
+    )
+    .define(
+      { exp: `exp '*' exp | exp '/' exp` },
+      valueReducer((values, { matched }) =>
+        matched[1].text == "*" ? values[0] * values[2] : values[0] / values[2]
+      )
+    )
+    .check(lexer.getTokenTypes())
+    .compile()
+);
 
-rl.on("line", function (line) {
-  let res = parser.parse(line + "\n");
-  if (res.length == 1 && res[0] instanceof ASTNode) {
-    console.log(res[0].data.value);
-    parser.reset();
-  }
-  rl.prompt();
-});
+function assertEqual(input: string, desired: number) {
+  parser.reset();
+  let res = parser.parse(input);
+  if (res.length != 1)
+    throw new Error(`Reduce failed for input "${input}". Result: ${res}`);
+  if (res[0].data.value != desired)
+    throw new Error(
+      `Wrong result. Input: "${input}", want: ${desired}, got: ${res[0].data.value}`
+    );
+}
 
-rl.prompt();
+assertEqual("1+1", 2);
+assertEqual("-1-2", -3);
+assertEqual("1 - -1", 2);
+assertEqual("2+3*4/5", 4.4);
+assertEqual("(2+3)*4/5", 4);
+
+console.log("All check passed.");
