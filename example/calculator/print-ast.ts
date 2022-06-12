8f7dd3ce6140d2877b9781e93a3ac5fa12ba3878
DiscreteTom <discrete_tom@outlook.com>
2022-06-12 11:24:35 +0800

    update example

--- example/calculator/print-ast.ts
+++ example/calculator/print-ast.ts
@@ -4,4 +4,4 @@ let res = parser.parse("(2+3)*4/5");
 if (!res.accept || res.buffer.length != 1)
   throw new Error(`Reduce failed for input. Result: ${res}`);
 
-console.log(res.buffer[0].toString());
+console.log(res.buffer[0].toTreeString());
