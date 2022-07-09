9bc0ecaf9b3ad67bf4d0803306e282a61558f477
DiscreteTom <discrete_tom@outlook.com>
2022-07-09 18:31:59 +0800

    chore(test): add string-literal test

--- example/string-literal.ts
+++ example/string-literal.ts
@@ -1,6 +1,6 @@
 import { Lexer } from "../src";
 
-let lexer = new Lexer.Builder()
+export let lexer = new Lexer.Builder()
   .ignore(/^\s/)
   .define({
     simpleStr: Lexer.stringLiteral({
@@ -9,35 +9,6 @@ let lexer = new Lexer.Builder()
     }),
     multilineStr: Lexer.stringLiteral({ back: true, multiline: true }),
     customQuote: Lexer.stringLiteral({ quote: "*" }),
-    custom: Lexer.stringLiteral({ from: "^", to: "$" }),
+    custom: Lexer.stringLiteral({ from: "^", to: "$$" }),
   })
   .build();
-
-function assertEq(input: string) {
-  let token = lexer.reset().lex(input);
-  if (token == null) throw new Error(`Unable to tokenize ${input}`);
-  if (token.content != input)
-    throw new Error(
-      `Mismatch, input=${input}, want=${input}, got=${token.content}`
-    );
-  console.log(token.content);
-}
-
-assertEq(`'123'`);
-assertEq(`'123\\'456'`);
-assertEq(`'123\\\\'`);
-assertEq(`"123"`);
-assertEq(`"123\\"456"`);
-assertEq(`"123\\\\"`);
-assertEq("`123`");
-assertEq("`123\n456`");
-assertEq("`123\\`456`");
-assertEq("`123\\\\`");
-assertEq(`*123*`);
-assertEq(`*123\\*456*`);
-assertEq(`*123\\\\*`);
-assertEq(`^123$`);
-assertEq(`^123\\$456$`);
-assertEq(`^123\\\\$`);
-
-console.log(`All check passed.`);
---
+++ test/string-literal.test.ts
@@ -0,0 +1,36 @@
+import { lexer } from "../example/string-literal";
+
+function assertEq(input: string) {
+  let token = lexer.reset().lex(input);
+  if (token == null) throw new Error(`Unable to tokenize ${input}`);
+  expect(token.content).toBe(input);
+}
+
+test("basic string", () => {
+  assertEq(`'123'`);
+  assertEq(`"123"`);
+  assertEq("`123`");
+});
+
+test("escaped string", () => {
+  assertEq(`'123\\'456'`);
+  assertEq(`'123\\\\'`);
+  assertEq(`"123\\"456"`);
+  assertEq(`"123\\\\"`);
+  assertEq("`123\n456`");
+  assertEq("`123\\`456`");
+  assertEq("`123\\\\`");
+});
+
+test("custom quotes", () => {
+  assertEq(`*123*`);
+  assertEq(`*123\\*456*`);
+  assertEq(`*123\\\\*`);
+});
+
+test("custom boundary", () => {
+  assertEq(`^123$$`);
+  assertEq(`^123\\$$456$$`);
+  assertEq(`^123$\\$456$$`);
+  assertEq(`^123\\\\$$`);
+});
