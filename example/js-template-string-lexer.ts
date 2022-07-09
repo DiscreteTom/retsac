205642040e5e1d4bd34575d4e1eb14a161683aa9
DiscreteTom <discrete_tom@outlook.com>
2022-07-09 15:50:18 +0800

    chore: update example

--- example/js-template-string-lexer.ts
+++ example/js-template-string-lexer.ts
@@ -2,44 +2,55 @@ import { Lexer } from "../src";
 
 let tempStrDepth = 0;
 
+function findUnescaped(s: string, target: string) {
+  for (let i = 0; i < s.length - target.length + 1; ++i) {
+    if (s[i] == "\\") {
+      i++; // escape next
+      continue;
+    }
+    if (s.slice(i, i + target.length) == target) return true;
+  }
+  return false;
+}
+
 let lexer = new Lexer.Builder()
   .ignore(/^\s/)
   .define({
-    tempStr: Lexer.from_to(
-      "`",
-      /(^`|[^\\]`)/, // '`' without escape
-      false
-    ).reject((s) => s.search(/[^\\]\$\{/) != -1), // reject if find '${`
-    tempStrLeft: Lexer.from_to(
-      "`",
-      /(^\$\{|[^\\]\$\{)/, // '${' without escape
-      false
-    ).then((_) => tempStrDepth++), // use closure to store state
-    tempStrRight: Lexer.from_to(
-      "}",
-      /(^`|[^\\]`)/, // '`' without escape
-      false
-    )
-      .reject((s) => tempStrDepth == 0 || s.search(/[^\\]\$\{/) != -1)
+    tempStr: Lexer.stringLiteral({ back: true, multiline: true }).reject(
+      (s) => findUnescaped(s, "${") // reject if find '${` without escape
+    ),
+    tempStrLeft: Lexer.stringLiteral({
+      from: "`",
+      to: "${",
+      multiline: true,
+    }).then((_) => tempStrDepth++), // use closure to store state
+    tempStrRight: Lexer.stringLiteral({ from: "}", to: "`", multiline: true })
+      .reject(
+        (s) =>
+          tempStrDepth == 0 || // not in template string
+          findUnescaped(s, "${") // contains another '${'
+      )
       .then((_) => tempStrDepth--),
-    tempStrMiddle: Lexer.from_to(
-      "}",
-      /(^\$\{|[^\\]\$\{)/, // '${' without escape
-      false
-    ).reject((_) => tempStrDepth == 0), // check state
-    word: /^\w+/,
-    string: Lexer.stringLiteral({ single: true }),
+    tempStrMiddle: Lexer.stringLiteral({
+      from: "}",
+      to: "${",
+      multiline: true,
+    }).reject((_) => tempStrDepth == 0), // check state
+    exp: /^\w+/,
+    simpleString: Lexer.stringLiteral({ single: true, double: true }),
   })
   .anonymous(Lexer.exact(..."+"))
   .build();
 
+// try to parse those
+[`123`, `123 ${lexer}  \${789} 0`, `123 ${"123" + `456 ${999}`} 789`];
+
 [
-  "`123`", // `123`
-  "`123 ${ lexer }  \\${789} 0`", // `123 ${ lexer }  \${789} 0`
-  "`123 ${ '123' + `456 ${ 999 }` } 789`", // `123 ${ '123' + `456 ${ 999 }` } 789`
+  "`123`",
+  "`123 ${ lexer }  \\${789} 0`",
+  "`123 ${ '123' + `456 ${ 999 }` } 789`",
 ].map((s) => {
-  lexer.reset();
-  let tokens = lexer.lexAll(s);
+  let tokens = lexer.reset().lexAll(s);
   console.log(`>>> Scanning: ${s} <<<`);
   console.log(tokens.map((t) => ({ type: t.type, content: t.content })));
   if (lexer.hasRest()) console.log(`Undigested: ${lexer.getRest()}`);
