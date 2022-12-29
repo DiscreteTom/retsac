846fdf88e5d889d5cb7844035302cd820618b425
DiscreteTom <discrete_tom@outlook.com>
2022-12-29 23:17:53 +0800

    chore: export LR.reducer

--- src/parser/LR/builder/index.ts
+++ src/parser/LR/builder/index.ts
@@ -6,3 +6,4 @@ export const callback = DefinitionContextBuilder.callback;
 export const rejecter = DefinitionContextBuilder.rejecter;
 export const resolveRS = DefinitionContextBuilder.resolveRS;
 export const resolveRR = DefinitionContextBuilder.resolveRR;
+export const reducer = DefinitionContextBuilder.reducer;
