2f10e9fb4be1d8ff13bfb6136e42a404b0e8535a
DiscreteTom <discrete_tom@outlook.com>
2022-12-29 23:50:01 +0800

    chore: add conflict checker test

--- tests/lr-parser.test.ts
+++ tests/lr-parser.test.ts
@@ -1,4 +1,5 @@
 import { LR } from "../src";
+import { ParserError } from "../src/parser/LR/error";
 
 test("R-S conflict", () => {
   expect(() => {
@@ -7,7 +8,7 @@ test("R-S conflict", () => {
       .define({ exp: `exp '+' exp` })
       .define({ exp: `exp '*'` })
       .checkConflicts();
-  }).toThrowError();
+  }).toThrow(ParserError);
 
   expect(() => {
     new LR.ParserBuilder<void>()
@@ -18,7 +19,7 @@ test("R-S conflict", () => {
       )
       .define({ exp: `exp '*'` })
       .checkConflicts();
-  }).not.toThrowError();
+  }).not.toThrow(ParserError);
 });
 
 test("R-R conflict", () => {
@@ -28,7 +29,7 @@ test("R-R conflict", () => {
       .define({ exp: `'-' exp` })
       .define({ exp: `exp '-' exp` })
       .checkConflicts();
-  }).toThrowError();
+  }).toThrow(ParserError);
 
   expect(() => {
     new LR.ParserBuilder<void>()
@@ -45,5 +46,44 @@ test("R-R conflict", () => {
         )
       )
       .checkConflicts();
-  }).not.toThrowError();
+  }).not.toThrow(ParserError);
+});
+
+test("conflict checker", () => {
+  expect(() => {
+    new LR.ParserBuilder<void>()
+      .entry("exp")
+      .define(
+        { exp: `'-' exp` },
+        LR.resolveRS({ exp: `exp '-' exp` }, { next: `'-'`, reject: false })
+      )
+      .define(
+        { exp: `exp '-' exp` },
+        LR.resolveRR<void>(
+          { exp: `'-' exp` },
+          { next: `'-'`, reject: false, handleEnd: true }
+        ).resolveRR(
+          { exp: `exp '-'` }, // non-existing grammar rule
+          { next: `'-'`, reject: false, handleEnd: true }
+        )
+      )
+      .checkConflicts();
+  }).toThrow(ParserError);
+
+  expect(() => {
+    new LR.ParserBuilder<void>()
+      .entry("exp")
+      .define(
+        { exp: `'-' exp` },
+        LR.resolveRS({ exp: `exp '-' exp` }, { next: `'-'`, reject: false })
+      )
+      .define(
+        { exp: `exp '-' exp` },
+        LR.resolveRR<void>(
+          { exp: `'-' exp` },
+          { next: `'-' '*'`, reject: false, handleEnd: true } // non-existing token
+        )
+      )
+      .checkConflicts();
+  }).toThrow(ParserError);
 });
