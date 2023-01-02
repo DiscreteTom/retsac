a965f4e9f8bbcf84404d1cd686a396e342f9b909
DiscreteTom <discrete_tom@outlook.com>
2023-01-02 18:04:40 +0800

    chore: fix typo

--- example/calculator/print-ast.ts
+++ example/calculator/print-ast.ts
@@ -3,7 +3,8 @@ import { manager } from "./core";
 const res = manager.parseAll("2+3*(4/5)");
 if (!res.accept || res.buffer.length != 1)
   throw new Error(
-    `Reduce failed for input. Result: ${res.buffer
+    `Reduce failed for input. Result: ${manager
+      .getBuffer()
       .map((node) => node.toString())
       .join(" ")}`
   );
