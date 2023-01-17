00efb1b58e1c50039b880276f40093e42f8098a2
DiscreteTom <discrete_tom@outlook.com>
2023-01-17 19:00:45 +0800

    tests: copy LR examples from ELR example

---
+++ example/calculator/core-lr.ts
@@ -0,0 +1,62 @@
+import { Lexer, LR } from "../../src";
+
+const lexer = new Lexer.Builder()
+  .ignore(/^\s/)
+  .define({
+    number: /^[0-9]+(?:\.[0-9]+)?/,
+  })
+  .anonymous(Lexer.exact(..."+-*/()"))
+  .build();
+
+export const parser = new LR.ParserBuilder<number>()
+  .entry("exp")
+  .define(
+    { exp: "number" },
+    LR.reducer((_, { matched }) => Number(matched[0].text))
+  )
+  .define(
+    { exp: `'-' exp` },
+    LR.reducer<number>((values) => -values[1]!)
+      .resolveRS({ exp: `exp '+' exp` }, { next: `'+'`, reduce: true })
+      .resolveRS({ exp: `exp '-' exp` }, { next: `'-'`, reduce: true })
+      .resolveRS({ exp: `exp '*' exp` }, { next: `'*'`, reduce: true })
+      .resolveRS({ exp: `exp '/' exp` }, { next: `'/'`, reduce: true })
+  )
+  .define(
+    { exp: `'(' exp ')'` },
+    LR.reducer((values) => values[1])
+  )
+  .define(
+    { exp: `exp '+' exp` },
+    LR.reducer<number>((values) => values[0]! + values[2]!)
+      .resolveRS({ exp: `exp '+' exp` }, { next: `'+'`, reduce: true })
+      .resolveRS({ exp: `exp '-' exp` }, { next: `'-'`, reduce: true })
+      .resolveRS({ exp: `exp '*' exp` }, { next: `'*'`, reduce: false })
+      .resolveRS({ exp: `exp '/' exp` }, { next: `'/'`, reduce: false })
+  )
+  .define(
+    { exp: `exp '-' exp` },
+    LR.reducer<number>((values) => values[0]! - values[2]!)
+      .resolveRS({ exp: `exp '+' exp` }, { next: `'+'`, reduce: true })
+      .resolveRS({ exp: `exp '-' exp` }, { next: `'-'`, reduce: true })
+      .resolveRS({ exp: `exp '*' exp` }, { next: `'*'`, reduce: false })
+      .resolveRS({ exp: `exp '/' exp` }, { next: `'/'`, reduce: false })
+  )
+  .define(
+    { exp: `exp '*' exp` },
+    LR.reducer<number>((values) => values[0]! * values[2]!)
+      .resolveRS({ exp: `exp '+' exp` }, { next: `'+'`, reduce: true })
+      .resolveRS({ exp: `exp '-' exp` }, { next: `'-'`, reduce: true })
+      .resolveRS({ exp: `exp '*' exp` }, { next: `'*'`, reduce: true })
+      .resolveRS({ exp: `exp '/' exp` }, { next: `'/'`, reduce: true })
+  )
+  .define(
+    { exp: `exp '/' exp` },
+    LR.reducer<number>((values) => values[0]! / values[2]!)
+      .resolveRS({ exp: `exp '+' exp` }, { next: `'+'`, reduce: true })
+      .resolveRS({ exp: `exp '-' exp` }, { next: `'-'`, reduce: true })
+      .resolveRS({ exp: `exp '*' exp` }, { next: `'*'`, reduce: true })
+      .resolveRS({ exp: `exp '/' exp` }, { next: `'/'`, reduce: true })
+  )
+  .checkAll(lexer.getTokenTypes(), lexer)
+  .build(lexer);
