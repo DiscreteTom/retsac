ad9340b51e0d59e1b03e1d4bbd7889faede8fa8e
DiscreteTom <discrete_tom@outlook.com>
2023-01-24 15:12:25 +0800

    chore: optimize export

--- src/parser/expect/builder/index.ts
+++ src/parser/expect/builder/index.ts
@@ -2,6 +2,7 @@ import { DefinitionContextBuilder } from "./ctx-builder";
 
 export * from "./builder";
 export * from "./ctx-builder";
+export * from "./mode";
 
 export const callback = DefinitionContextBuilder.callback;
 export const rejecter = DefinitionContextBuilder.rejecter;
--- src/parser/expect/model/index.ts
+++ src/parser/expect/model/index.ts
@@ -1,1 +1,3 @@
 export * from "./context";
+export * from "./grammar";
+export * from "./re-lex";
