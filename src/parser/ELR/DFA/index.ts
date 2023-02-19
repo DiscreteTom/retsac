509744a26e247eadda985c790d65dbefd3277ab0
DiscreteTom <discrete_tom@outlook.com>
2023-02-19 16:48:58 +0800

    fix: export name

--- src/parser/ELR/DFA/index.ts
+++ src/parser/ELR/DFA/index.ts
@@ -1,4 +1,4 @@
-export { DFA as BaseDFA } from "./DFA";
+export { DFA } from "./DFA";
 export { State } from "./state";
 export { Candidate } from "./candidate";
 export { DFABuilder } from "./builder";
