47935095545d13327a37e4b31d3307ce0288529a
DiscreteTom <discrete_tom@outlook.com>
2023-03-10 21:23:50 +0800

    chore: optimize code using high level API

--- src/parser/ELR/advanced/utils/resolvers.ts
+++ src/parser/ELR/advanced/utils/resolvers.ts
@@ -2,54 +2,10 @@ import { IParserBuilder } from "../../model/builder";
 
 export function applyResolvers<T>(builder: IParserBuilder<T>) {
   return builder
-    .resolveRS(
-      { gr: `gr '|' gr` },
-      { gr: `gr '?'` },
-      { next: `'?'`, reduce: false }
-    )
-    .resolveRS(
-      { gr: `gr '|' gr` },
-      { gr: `gr '*'` },
-      { next: `'*'`, reduce: false }
-    )
-    .resolveRS(
-      { gr: `gr '|' gr` },
-      { gr: `gr '+'` },
-      { next: `'+'`, reduce: false }
-    )
-    .resolveRS(
-      { gr: `gr '|' gr` },
-      { gr: `gr '|' gr` },
-      { next: `'|'`, reduce: true }
-    )
-    .resolveRS(
-      { gr: `gr '|' gr` },
+    .priority(
+      [{ gr: `gr '?'` }, { gr: `gr '*'` }, { gr: `gr '+'` }],
       { gr: `gr gr` },
-      { next: `gr grammar literal '('`, reduce: false }
+      { gr: `gr '|' gr` }
     )
-    .resolveRS(
-      { gr: `gr gr` },
-      { gr: `gr '?'` },
-      { next: `'?'`, reduce: false }
-    )
-    .resolveRS(
-      { gr: `gr gr` },
-      { gr: `gr '*'` },
-      { next: `'*'`, reduce: false }
-    )
-    .resolveRS(
-      { gr: `gr gr` },
-      { gr: `gr '+'` },
-      { next: `'+'`, reduce: false }
-    )
-    .resolveRS(
-      { gr: `gr gr` },
-      { gr: `gr '|' gr` },
-      { next: `'|'`, reduce: true }
-    )
-    .resolveRS(
-      { gr: `gr gr` },
-      { gr: `gr gr` },
-      { next: `gr grammar literal '('`, reduce: true }
-    );
+    .leftSA({ gr: `gr '|' gr` }, { gr: `gr gr` });
 }
