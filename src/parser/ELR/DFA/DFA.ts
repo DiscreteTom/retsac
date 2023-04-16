3aa948f2beb93b7d83eb865789e664a5d9fedfeb
DiscreteTom <discrete_tom@outlook.com>
2023-04-16 17:39:12 +0800

    refactor(ELR): remove DFA.stateStack

--- src/parser/ELR/DFA/DFA.ts
+++ src/parser/ELR/DFA/DFA.ts
@@ -8,8 +8,6 @@ import { State } from "./state";
 
 /** DFA for ELR parsers. Stateless. */
 export class DFA<T> {
-  /** Current state is `states.at(-1)`. */
-  protected stateStack: State<T>[];
   debug: boolean;
 
   constructor(
@@ -21,19 +19,12 @@ export class DFA<T> {
     private readonly firstSets: ReadonlyMap<string, GrammarSet>,
     /** `Grammar => Grammars` */
     protected readonly followSets: ReadonlyMap<string, GrammarSet>,
-    /** string representation of candidate => candidate */
+    /** `string representation of candidate => candidate` */
     protected readonly allInitialCandidates: ReadonlyMap<string, Candidate<T>>,
-    /** string representation of state => state */
-    protected readonly allStatesCache: Map<string, State<T>>,
+    /** `string representation of state => state` */
+    protected readonly allStates: Map<string, State<T>>,
     private readonly cascadeQueryPrefix: string | undefined
-  ) {
-    this.reset();
-  }
-
-  reset() {
-    // reset state stack with entry state
-    this.stateStack = [this.entryState];
-  }
+  ) {}
 
   getFirstSets() {
     return this.firstSets;
@@ -61,13 +52,13 @@ export class DFA<T> {
 
     while (true) {
       let changed = false;
-      this.allStatesCache.forEach((state) => {
+      this.allStates.forEach((state) => {
         mockNodes.forEach((node) => {
           if (
             state.getNext(
               node,
               this.NTClosures,
-              this.allStatesCache,
+              this.allStates,
               this.allInitialCandidates
             ).changed
           )
@@ -84,7 +75,7 @@ export class DFA<T> {
    */
   getAllStates() {
     const result: State<T>[] = [];
-    this.allStatesCache.forEach((s) => result.push(s));
+    this.allStates.forEach((s) => result.push(s));
     return result;
   }
 
@@ -97,7 +88,9 @@ export class DFA<T> {
     commitParser: () => void,
     stopOnError = false
   ): { output: ParserOutput<T>; lexer: ILexer } {
-    this.reset();
+    // reset state stack with entry state
+    /** Current state is `states.at(-1)`. */
+    let stateStack = [this.entryState];
 
     let index = 0; // buffer index
     let errors: ASTNode<T>[] = [];
@@ -115,7 +108,7 @@ export class DFA<T> {
       }
 
       // apply state
-      this.stateStack = state!.stateStack;
+      stateStack = state!.stateStack;
       buffer = state!.buffer;
       lexer = state!.lexer;
       index = state!.index;
@@ -132,7 +125,7 @@ export class DFA<T> {
     while (true) {
       if (index >= buffer.length) {
         // end of buffer, try to lex input string to get next ASTNode
-        const res = this.stateStack.at(-1)!.tryLex(lexer, this.followSets);
+        const res = stateStack.at(-1)!.tryLex(lexer, this.followSets);
         // if no more ASTNode can be lexed
         if (res.length == 0) {
           // try to restore from re-lex stack
@@ -144,7 +137,7 @@ export class DFA<T> {
               console.log(
                 `[End] No matching token can be lexed. Rest of input: ${lexer
                   .getRest()
-                  .slice(0, 10)}\nCandidates:\n${this.stateStack
+                  .slice(0, 10)}\nCandidates:\n${stateStack
                   .at(-1)!
                   .candidates.map((c) => c.toString())
                   .join("\n")}`
@@ -156,7 +149,7 @@ export class DFA<T> {
           // we need to append reLexStack reversely, so that the first lexing result is at the top of the stack
           for (let i = res.length - 1; i >= 0; --i) {
             reLexStack.push({
-              stateStack: this.stateStack.slice(),
+              stateStack: stateStack.slice(),
               buffer: buffer.slice().concat(res[i].node),
               lexer: res[i].lexer,
               index,
@@ -172,12 +165,12 @@ export class DFA<T> {
       }
 
       // try to construct next state
-      const nextStateResult = this.stateStack
+      const nextStateResult = stateStack
         .at(-1)!
         .getNext(
           buffer[index],
           this.NTClosures,
-          this.allStatesCache,
+          this.allStates,
           this.allInitialCandidates
         );
       if (nextStateResult.state == null) {
@@ -191,7 +184,7 @@ export class DFA<T> {
             console.log(
               `[End] No more candidate. Node=${buffer
                 .at(-1)
-                ?.toString()} Candidates:\n${this.stateStack
+                ?.toString()} Candidates:\n${stateStack
                 .at(-1)!
                 .candidates.map((c) => c.toString())
                 .join("\n")}`
@@ -201,10 +194,10 @@ export class DFA<T> {
       }
 
       // next state exist, push stack
-      this.stateStack.push(nextStateResult.state);
+      stateStack.push(nextStateResult.state);
 
       // try reduce with the new state
-      const { res, rollback, context, commit } = this.stateStack
+      const { res, rollback, context, commit } = stateStack
         .at(-1)!
         .tryReduce(
           buffer,
@@ -230,7 +223,7 @@ export class DFA<T> {
       index -= reduced - 1; // digest n, generate 1
       buffer = res.buffer;
       errors.push(...res.errors);
-      for (let i = 0; i < reduced; ++i) this.stateStack.pop(); // remove the reduced states
+      for (let i = 0; i < reduced; ++i) stateStack.pop(); // remove the reduced states
       // if a top-level NT is reduced to the head of the buffer, should return
       if (this.entryNTs.has(buffer[0].type) && index == 0)
         return { output: { accept: true, buffer, errors }, lexer };
