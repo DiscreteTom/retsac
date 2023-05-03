423946b721cc4d60a52bec1131e99e922d221b1b
DiscreteTom <discrete_tom@outlook.com>
2023-05-03 15:17:19 +0800

    tests: update lexer/utils.test

--- tests/lexer/utils.test.ts
+++ tests/lexer/utils.test.ts
@@ -16,6 +16,7 @@ test("lexer utils fromTo", () => {
   expect(lexer.reset().lex("c ")?.content).toBe("c ");
   expect(lexer.reset().lex("ef")?.content).toBe("ef");
   expect(lexer.reset().lex("e  f")?.content).toBe("e  f");
+  expect(lexer.reset().lex("e")).toBe(null);
 });
 
 test("lexer utils exact", () => {
@@ -50,44 +51,54 @@ test("lexer utils wordType", () => {
 test("lexer utils stringLiteral", () => {
   const lexer = new Lexer.Builder()
     .define({
-      str1: stringLiteral(`'`, { acceptUnclosed: false }).or(
-        stringLiteral(`"`, { acceptUnclosed: false })
-      ),
-      str2: stringLiteral("`", { multiline: true, acceptUnclosed: false }),
-      str3: stringLiteral("a", { close: "b", acceptUnclosed: false }),
-      str4: stringLiteral("&", { acceptUnclosed: false }),
+      string: stringLiteral(`'`)
+        .or(stringLiteral(`"`, { escape: false }))
+        .or(stringLiteral("`", { multiline: true }))
+        .or(stringLiteral("a", { close: "b" }))
+        .or(stringLiteral("c", { unclosedError: "my error" }))
+        .or(stringLiteral("d", { acceptUnclosed: false }))
+        .or(stringLiteral("e", { multiline: true, escape: false }))
+        .or(
+          stringLiteral("f", {
+            multiline: true,
+            escape: false,
+            acceptUnclosed: false,
+          })
+        ),
     })
     .build();
 
-  expect(lexer.reset().lex(`""`)?.content).toBe(`""`);
-  expect(lexer.reset().lex(`"123"`)?.content).toBe(`"123"`);
-  expect(lexer.reset().lex(`"123\\""`)?.content).toBe(`"123\\""`);
-  expect(lexer.reset().lex(`"123\n\\""`)).toBe(null);
-  expect(lexer.reset().lex(`''`)?.content).toBe(`''`);
+  // simple string
   expect(lexer.reset().lex(`'123'`)?.content).toBe(`'123'`);
+  // accept escaped by default
   expect(lexer.reset().lex(`'123\\''`)?.content).toBe(`'123\\''`);
+  // reject multiline but accept unclosed by default
+  const token1 = lexer.reset().lex(`'123\n123'`);
+  expect(token1?.content).toBe(`'123\n`);
+  expect(token1?.error).toBe("unclosed string literal");
+  // accept unclosed by default
+  const token2 = lexer.reset().lex(`'123`);
+  expect(token2?.content).toBe(`'123`);
+  expect(token2?.error).toBe("unclosed string literal");
+  // disable escaped
+  expect(lexer.reset().lex(`"123"`)?.content).toBe(`"123"`);
+  expect(lexer.reset().lex(`"123\\""`)?.content).toBe(`"123\\"`);
+  expect(lexer.reset().lex(`"123\n"`)?.error).toBe(`unclosed string literal`);
+  // enable multiline
+  expect(lexer.reset().lex("`123`")?.content).toBe("`123`");
   expect(lexer.reset().lex("`123\n123`")?.content).toBe("`123\n123`");
-  expect(lexer.reset().lex("ab")?.content).toBe("ab");
+  // customize border
   expect(lexer.reset().lex("a123b")?.content).toBe("a123b");
-  expect(lexer.reset().lex("a123\\bb")?.content).toBe("a123\\bb");
-  expect(lexer.reset().lex("&&")?.content).toBe("&&");
-  expect(lexer.reset().lex("&123&")?.content).toBe("&123&");
-  expect(lexer.reset().lex("&123\\&&")?.content).toBe("&123\\&&");
-
-  // EOF
-  expect(lexer.reset().lex(`"`)).toBe(null);
-  expect(lexer.reset().lex(`"123`)).toBe(null);
-  expect(lexer.reset().lex(`"123\\"`)).toBe(null);
-  expect(lexer.reset().lex(`'`)).toBe(null);
-  expect(lexer.reset().lex(`'123`)).toBe(null);
-  expect(lexer.reset().lex(`'123\\'`)).toBe(null);
-  expect(lexer.reset().lex("`123\n123")).toBe(null);
-  expect(lexer.reset().lex("a")).toBe(null);
-  expect(lexer.reset().lex("a123")).toBe(null);
-  expect(lexer.reset().lex("a123\\b")).toBe(null);
-  expect(lexer.reset().lex("&")).toBe(null);
-  expect(lexer.reset().lex("&123")).toBe(null);
-  expect(lexer.reset().lex("&123\\&")).toBe(null);
+  // accept unclosed, customize error
+  expect(lexer.reset().lex("c123")?.error).toBe("my error");
+  // reject unclosed
+  expect(lexer.reset().lex("d123")).toBe(null);
+  // not escape, but multiline
+  expect(lexer.reset().lex("e123\ne")?.content).toBe("e123\ne");
+  expect(lexer.reset().lex("e123\n\\ee")?.content).toBe("e123\n\\e");
+  expect(lexer.reset().lex("e123\n123")?.content).toBe("e123\n123");
+  // not escape, multiline, reject unclosed
+  expect(lexer.reset().lex("f123\n123")).toBe(null);
 });
 
 test("lexer utils whitespaces", () => {
