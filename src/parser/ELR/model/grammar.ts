edc5d692479628a6bb930ee101c8ec36e5bdf2bb
DiscreteTom <discrete_tom@outlook.com>
2023-05-06 16:50:40 +0800

    chore(ELR): use JSON.stringify for grammar.toStr

--- src/parser/ELR/model/grammar.ts
+++ src/parser/ELR/model/grammar.ts
@@ -87,7 +87,7 @@ export class Grammar {
       (this.str =
         this.type == GrammarType.LITERAL
           ? // literal content
-            `'${this.content}'`
+            JSON.stringify(this.content)
           : // type name
             this.content)
     );
