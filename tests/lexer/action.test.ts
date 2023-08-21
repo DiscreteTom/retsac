ddd1eb6c4fd64ed2848f790ce6dc6762f89cbd21
DiscreteTom <discrete_tom@outlook.com>
2023-08-21 19:20:36 +0800

    tests(lexer): update action.test

--- tests/lexer/action.test.ts
+++ tests/lexer/action.test.ts
@@ -1,217 +1,172 @@
-import { Action, ActionAcceptedOutput, ActionOutput } from "../../src/lexer";
+import {
+  Action,
+  AcceptedActionOutput,
+  ActionInput,
+  ActionSource,
+} from "../../src/lexer";
+
+function expectAccept(
+  buffer: string,
+  src: ActionSource,
+  override?: Partial<AcceptedActionOutput>
+) {
+  const input = new ActionInput({ buffer, start: 0 });
+  const action = Action.from(src);
+  const output = action.exec(input) as AcceptedActionOutput;
+  expect(output.accept).toBe(true);
+  expect(output.buffer).toBe(override?.buffer ?? buffer);
+  expect(output.start).toBe(override?.start ?? 0);
+  expect(output.digested).toBe(override?.digested ?? buffer.length);
+  expect(output.content).toBe(override?.content ?? buffer);
+  expect(output.rest).toBe(override?.rest ?? "");
+  expect(output.error).toBe(override?.error ?? undefined);
+  expect(output.muted).toBe(override?.muted ?? false);
+}
+
+function expectReject(buffer: string, src: ActionSource) {
+  const input = new ActionInput({ buffer, start: 0 });
+  const action = Action.from(src);
+  const output = action.exec(input);
+  expect(output.accept).toBe(false);
+}
 
 describe("Lexer action constructor", () => {
   test("from simple", () => {
-    const input = "123";
-    expect(Action.from((buffer) => buffer.length).exec(input)).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
+    const buffer = "123";
+    expectAccept(buffer, ({ buffer }) => buffer); // return string
+    expectAccept(buffer, ({ buffer }) => buffer.length); // return number
+    // simple accepted output
+    expectAccept(buffer, ({ buffer }) => ({ content: buffer }));
+    expectAccept(buffer, ({ buffer }) => ({ digested: buffer.length }));
+    expectAccept(buffer, ({ buffer }) => ({
+      digested: buffer.length,
+      content: buffer,
       error: undefined,
-    } as ActionOutput);
+      rest: "",
+      muted: false,
+    }));
   });
 
   test("from regex", () => {
-    const input = "   ";
-    expect(Action.from(/^\s*/).exec(input)).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    const buffer = "   ";
+    expectAccept(buffer, /^\s*/);
   });
 
   test("from another action", () => {
-    const input = "   ";
-    const another = Action.from(/^\s*/);
-    expect(Action.from(another).exec(input)).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    const buffer = "   ";
+    expectAccept(buffer, Action.from(/^\s*/));
   });
 
-  test("from exec", () => {
-    const input = "   ";
-    const output = {
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput;
-    expect(new Action((buffer) => output).exec(input)).toEqual(output);
+  test("action constructor", () => {
+    const buffer = "   ";
+    expectAccept(
+      buffer,
+      new Action(
+        ({ buffer }) =>
+          new AcceptedActionOutput({
+            digested: buffer.length,
+            content: buffer,
+            buffer,
+            start: 0,
+            muted: false,
+          })
+      )
+    );
   });
 });
 
 describe("Action decorator", () => {
   test("muted action", () => {
-    const input = "   ";
+    const buffer = "   ";
 
     // muted
-    expect(Action.from(/^\s*/).mute().exec(input)).toEqual({
-      accept: true,
-      muted: true,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    expectAccept(buffer, Action.from(/^\s*/).mute(), { muted: true });
 
     // not muted
-    expect(Action.from(/^\s*/).mute(false).exec(input)).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    expectAccept(buffer, Action.from(/^\s*/).mute(false));
 
     // muted with a function
-    expect(
-      Action.from(/^\s*/)
-        .mute((content) => content == input)
-        .exec(input)
-    ).toEqual({
-      accept: true,
-      muted: true,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    expectAccept(
+      buffer,
+      Action.from(/^\s*/).mute(({ content }) => content == buffer),
+      { muted: true }
+    );
 
     // not muted with a function
-    expect(
-      Action.from(/^\s*/)
-        .mute((content) => content != input)
-        .exec(input)
-    ).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    expectAccept(
+      buffer,
+      Action.from(/^\s*/).mute(({ content }) => content != buffer)
+    );
 
     // not matched
-    expect(Action.from(/^123/).mute().exec(input)).toEqual({
-      accept: false,
-    } as ActionOutput);
+    expectReject(buffer, Action.from(/^123/).mute());
   });
 
   test("check action", () => {
-    const input = "   ";
+    const buffer = "   ";
     const errMsg = "err msg";
-    expect(
-      Action.from(/^\s*/)
-        .check((content) => (content == input ? undefined : errMsg))
-        .exec(input)
-    ).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    expectAccept(
+      buffer,
+      Action.from(/^\s*/).check(({ content }) =>
+        content == buffer ? undefined : errMsg
+      )
+    );
 
-    expect(
-      Action.from(/^123/)
-        .check((content) => (content == input ? undefined : errMsg))
-        .exec(input)
-    ).toEqual({ accept: false } as ActionOutput);
+    expectReject(
+      buffer,
+      Action.from(/^123/).check(({ content }) =>
+        content == buffer ? undefined : errMsg
+      )
+    );
 
-    expect(
-      Action.from(/^\s*/)
-        .check((content) => (content == input ? errMsg : undefined))
-        .exec(input)
-    ).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: errMsg,
-    } as ActionOutput);
+    expectAccept(
+      buffer,
+      Action.from(/^\s*/).check(({ content }) =>
+        content == buffer ? errMsg : undefined
+      ),
+      { error: errMsg }
+    );
   });
 
   test("error action", () => {
     // if accept, set error
-    expect(
-      (Action.from(/^123/).error("msg").exec("123") as ActionAcceptedOutput)
-        .error
-    ).toBe("msg");
+    expectAccept("123", Action.from(/^123/).error("msg"), { error: "msg" });
 
     // if reject, do nothing
-    expect(Action.from(/^123/).error("msg").exec("456")).toEqual({
-      accept: false,
-    });
+    expectReject("456", Action.from(/^123/).error("msg"));
   });
 
   test("reject action", () => {
-    const input = "   ";
+    const buffer = "   ";
 
     // accept
-    expect(Action.from(/^\s*/).exec(input)).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
+    expectAccept(buffer, Action.from(/^\s*/));
 
     // reject with a function
-    expect(
-      Action.from(/^\s*/)
-        .reject((content) => content == input)
-        .exec(input)
-    ).toEqual({ accept: false } as ActionOutput);
+    expectReject(
+      buffer,
+      Action.from(/^\s*/).reject(({ content }) => content == buffer)
+    );
 
     // directly reject
-    expect(Action.from(/^\s*/).reject().exec(input)).toEqual({
-      accept: false,
-    } as ActionOutput);
+    expectReject(buffer, Action.from(/^\s*/).reject());
 
     // reject with a value
-    expect(Action.from(/^\s*/).reject(true).exec(input)).toEqual({
-      accept: false,
-    } as ActionOutput);
-    expect(Action.from(/^\s*/).reject(false).exec(input).accept).toBe(true);
+    expectReject(buffer, Action.from(/^\s*/).reject(true));
+    expectAccept(buffer, Action.from(/^\s*/).reject(false));
 
     // if already rejected, do nothing
-    expect(Action.from(/^123/).reject().exec("456")).toEqual({
-      accept: false,
-    });
+    expectReject(buffer, Action.from(/^123/).reject());
   });
 
   test("then action", () => {
-    const input = "   ";
+    const buffer = "   ";
     let result = "";
 
-    const output = Action.from(/^\s*/)
-      .then((content) => (result = content))
-      .exec(input);
-
-    expect(output).toEqual({
-      accept: true,
-      muted: false,
-      digested: input.length,
-      content: input,
-      rest: "",
-      error: undefined,
-    } as ActionOutput);
-    expect(result).toBe(input);
+    expectAccept(
+      buffer,
+      Action.from(/^\s*/).then(({ content }) => (result = content))
+    );
+    expect(result).toBe(buffer);
   });
 });
