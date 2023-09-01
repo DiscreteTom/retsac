c22ad2290eef00ee546256b2af4b0dfbf4b29258
DiscreteTom <discrete_tom@outlook.com>
2023-09-01 21:21:56 +0800

    chore(parser): optimize code

--- src/parser/model.ts
+++ src/parser/model.ts
@@ -1,44 +1,73 @@
 import { Logger } from "../model";
 import { ASTNode } from "./ast";
 
-export type ParserAcceptedOutput<T> = {
+export type AcceptedParserOutput<T> = {
   accept: true;
-  /** Result AST nodes. */
+  /**
+   * Current AST nodes.
+   */
   buffer: readonly ASTNode<T>[];
-  /** Empty list if no error. */
-  errors: readonly ASTNode<T>[];
+  /**
+   * Empty list if no error.
+   */
+  errors: readonly ASTNode<T>[]; // TODO: newly added?
 };
 
-export const parserRejectedOutput = Object.freeze({ accept: false });
+export const rejectedParserOutput = Object.freeze({ accept: false });
 
 export type ParserOutput<T> =
-  | typeof parserRejectedOutput
-  | ParserAcceptedOutput<T>;
+  | typeof rejectedParserOutput
+  | AcceptedParserOutput<T>;
 
+/**
+ * The `input` will be fed to the lexer.
+ */
 export type ParseExec<T> = (
+  // TODO: what's the error?
   input?: string | { input?: string; stopOnError?: boolean }
 ) => ParserOutput<T>;
 
+// TODO: default T
 export interface IParser<T> {
   debug: boolean;
   logger: Logger;
-  /** Reset state. */
+  /**
+   * Reset state.
+   */
   reset(): this;
-  /** Clone a new parser with the same states. */
+  /**
+   * Clone a new parser with the same states.
+   */
   clone(options?: { debug?: boolean; logger?: Logger }): IParser<T>;
-  /** Clone a new parser without states. */
+  /**
+   * Clone a new parser without states.
+   */
   dryClone(options?: { debug?: boolean; logger?: Logger }): IParser<T>;
-  /** Feed a string to the lexer. */
+  /**
+   * Feed a string to the lexer.
+   */
   feed(input: string): this;
-  /** Try to yield an entry NT. */
-  parse: ParseExec<T>;
-  /** Try to reduce till the parser can't accept more. */
-  parseAll: ParseExec<T>;
-  /** Get error AST nodes. */
+  /**
+   * Try to yield an entry NT.
+   * Stop when the first entry NT is reduced.
+   */
+  readonly parse: ParseExec<T>;
+  /**
+   * Try to reduce till the parser can't accept more.
+   * This is useful if your entry NT can also be reduced by other rules.
+   */
+  readonly parseAll: ParseExec<T>;
+  /**
+   * Get error AST nodes.
+   */
   getErrors(): readonly ASTNode<T>[];
   hasErrors(): boolean;
-  /** Get all reduced AST nodes. */
-  getNodes(): readonly ASTNode<T>[];
-  /** Take the first AST node. */
-  take(): ASTNode<T> | undefined;
+  /**
+   * Current AST nodes.
+   */
+  getNodes(): readonly ASTNode<T>[]; // TODO: rename to buffer
+  /**
+   * Take the first AST node.
+   */
+  take(): ASTNode<T> | undefined; // TODO: take N
 }
