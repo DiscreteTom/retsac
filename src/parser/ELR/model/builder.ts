fe3f8d20af2a514d48162a3df4a48e59888cf800
DiscreteTom <discrete_tom@outlook.com>
2023-09-02 17:18:00 +0800

    chore(parser): update comments

--- src/parser/ELR/model/builder.ts
+++ src/parser/ELR/model/builder.ts
@@ -1,5 +1,4 @@
 import { ILexer } from "../../../lexer";
-import { Logger } from "../../../model";
 import { IParser } from "../../model";
 import {
   Definition,
@@ -8,62 +7,100 @@ import {
   RS_ResolverOptions,
 } from "../builder";
 
-export type BuildOptions = {
-  debug?: boolean;
-  logger?: Logger;
+export type BuildOptions = Partial<Pick<IParser<any>, "logger" | "debug">> & {
+  /**
+   * Which format to generate resolvers.
+   * If `undefined`, resolvers will not be generated.
+   *
+   * Default: `undefined`.
+   */
   generateResolvers?: "builder" | "context";
-  /** If `printAll` is true, print all errors instead of throwing errors. */
+  /**
+   * If `printAll` is true, print all errors instead of throwing errors.
+   *
+   * Default: `false`.
+   */
   printAll?: boolean;
+  /**
+   * Default: `false`.
+   */
   checkSymbols?: boolean;
+  /**
+   * Default: `false`.
+   */
   checkConflicts?: boolean;
+  /**
+   * Short for `checkSymbols` and `checkConflicts`.
+   *
+   * Default: `false`.
+   */
   checkAll?: boolean;
   /**
-   * If `true` (`false` by default), you can use `rollback` in your `DefinitionContextBuilder`.
+   * If `true`, the `rollback` in `DefinitionContextBuilder` will be used.
+   *
+   * Default: `false` to optimize performance.
    */
   rollback?: boolean;
   /**
-   * If `true` (by default), the parser will try to re-lex the input.
+   * If `true`, the parser will try to re-lex the input.
+   *
+   * Default: `true`.
    */
   reLex?: boolean;
 };
 
 export interface IParserBuilder<T> {
-  /** Declare top-level NT's. */
+  /**
+   * Declare top-level NT's.
+   * This is required for ELR parser.
+   * You should call this only once. If you call this multiple times, the last one will be used.
+   */
   entry(...defs: string[]): this;
+  /**
+   * Declare grammar rules.
+   */
   define(defs: Definition, ctxBuilder?: DefinitionContextBuilder<T>): this;
-  /** Generate the ELR parser. */
-  build(lexer: ILexer<any>, options?: BuildOptions): IParser<T>;
-  /** Resolve a reduce-shift conflict. */
+  /**
+   * Generate the ELR parser.
+   */
+  build(lexer: ILexer<any>, options?: BuildOptions): IParser<T>; // TODO: use generic type
+  /**
+   * Resolve a reduce-shift conflict.
+   */
   resolveRS(
     reducerRule: Definition,
     anotherRule: Definition,
     options: RS_ResolverOptions<T>
   ): this;
-  /** Resolve a reduce-reduce conflict. */
+  /**
+   * Resolve a reduce-reduce conflict.
+   */
   resolveRR(
     reducerRule: Definition,
     anotherRule: Definition,
     options: RR_ResolverOptions<T>
   ): this;
-  /** Apply a function to this builder. */
+  /**
+   * Apply a function to this builder.
+   */
   use(f: BuilderDecorator<T>): this;
   /**
-   * Generate resolvers to implement grammar rules' priorities.
+   * Generate resolvers by grammar rules' priorities.
    *
    * ```ts
    * // gr1 > gr2 = gr3
    * builder.priority(gr1, [gr2, gr3])
    * ```
    *
-   * Grammar rules with higher priority will always be reduced first,
-   * and grammar rules with the same priority will be reduced from left to right.
+   * Grammar rules with higher priority will always be accepted first,
+   * and grammar rules with the same priority will be accepted according to the order of definition you provided here.
    */
   priority(...defs: (Definition | Definition[])[]): this;
   /**
    * Generate resolvers to make these definitions left-self-associative.
    *
    * ```ts
-   * // 1 - 2 - 3 means (1 - 2) - 3 instead of 1 - (2 - 3)
+   * // `1 - 2 - 3` means `(1 - 2) - 3` instead of `1 - (2 - 3)`
    * builder.leftSA({ exp: `exp '-' exp` })
    * ```
    */
@@ -71,7 +108,7 @@ export interface IParserBuilder<T> {
   /**
    * Generate resolvers to make these definitions right-self-associative.
    * ```ts
-   * // a = b = 1 means a = (b = 1) instead of (a = b) = 1
+   * // `a = b = 1` means `a = (b = 1)` instead of `(a = b) = 1`
    * builder.rightSA({ exp: `var '=' exp` })
    * ```
    */
