4ed67603e52fd66b81604012fcd4caee104fb322
DiscreteTom <discrete_tom@outlook.com>
2023-09-03 23:16:09 +0800

    chore(parser): optimize code

--- src/parser/ELR/builder/model/temp-grammar.ts
+++ src/parser/ELR/builder/model/temp-grammar.ts
@@ -1,58 +1,36 @@
 import { ILexer } from "../../../../lexer";
-import { ASTNode, Traverser } from "../../../ast";
-import {
-  Grammar,
-  Callback,
-  GrammarRule,
-  GrammarType,
-  Condition,
-  GrammarRepo,
-} from "../../model";
+import { Traverser } from "../../../ast";
+import { Callback, GrammarRule, Condition, GrammarRepo } from "../../model";
 
-/** Grammar type, but can't distinguish N or NT. */
+/**
+ * Grammar type, but can't distinguish N or NT.
+ */
 export enum TempGrammarType {
   LITERAL,
-  /** T or NT */
+  /**
+   * For T/NT.
+   */
   GRAMMAR,
 }
 
-/** Grammar, but can't distinguish N or NT. */
+/**
+ * Grammar, but can't distinguish N or NT.
+ */
 export class TempGrammar {
   readonly type: TempGrammarType;
-  /** Literal content, or T/NT's type name. */
+  /**
+   * Literal content, or T/NT's type name.
+   */
   readonly content: string;
-  /** The name of the grammar. By default the value is equal to the type name(this.content). */
+  /**
+   * The name of the grammar. By default the value is equal to the type name(this.content).
+   */
   readonly name: string;
 
   constructor(p: Pick<TempGrammar, "type" | "content" | "name">) {
-    Object.assign(this, p);
-  }
-
-  static from(g: Readonly<Grammar>) {
-    return new TempGrammar({
-      type:
-        g.type == GrammarType.LITERAL
-          ? TempGrammarType.LITERAL
-          : TempGrammarType.GRAMMAR,
-      content: g.kind,
-      name: g.name,
-    });
-  }
-
-  eq<_>(g: Readonly<TempGrammar> | Readonly<ASTNode<_>> | Readonly<Grammar>) {
-    if (g instanceof TempGrammar)
-      return this.type == g.type && this.content == g.content;
-    else if (g instanceof ASTNode)
-      return this.type == TempGrammarType.LITERAL
-        ? // check literal content
-          this.content == g.text
-        : // check type name
-          this.content == g.kind;
-    else if (g instanceof Grammar)
-      return (
-        (this.type == TempGrammarType.LITERAL) ==
-          (g.type == GrammarType.LITERAL) && this.content == g.kind
-      );
+    this.type == p.type;
+    this.content = p.content;
+    this.name = p.name;
   }
 
   toGrammar(
@@ -88,10 +66,14 @@ export class TempGrammar {
   }
 }
 
-/** Grammar rule, but can't distinguish N or NT. */
+/**
+ * Grammar rule, but can't distinguish N or NT.
+ */
 export class TempGrammarRule<T> {
   readonly rule: readonly TempGrammar[];
-  /** The reduce target. */
+  /**
+   * The reduce target.
+   */
   readonly NT: string;
   callback?: Callback<T>;
   rejecter?: Condition<T>;
@@ -100,31 +82,24 @@ export class TempGrammarRule<T> {
   traverser?: Traverser<T>;
 
   constructor(
-    data: Partial<
-      Pick<
-        TempGrammarRule<T>,
-        "callback" | "rejecter" | "rollback" | "traverser"
-      >
-    > &
-      Pick<TempGrammarRule<T>, "rule" | "NT" | "commit">
+    data: Pick<
+      TempGrammarRule<T>,
+      | "rule"
+      | "NT"
+      | "commit"
+      | "callback"
+      | "rejecter"
+      | "rollback"
+      | "traverser"
+    >
   ) {
-    Object.assign(this, data);
-  }
-
-  /** Only check whether NT and rules are equal. */
-  weakEq<T>(rule: Readonly<TempGrammarRule<T>> | Readonly<GrammarRule<T>>) {
-    return (
-      this.NT == rule.NT &&
-      this.rule.length == rule.rule.length &&
-      this.rule.every((g, i) => g.eq(rule.rule[i]))
-    );
-  }
-
-  toString() {
-    return GrammarRule.getString({
-      NT: this.NT,
-      rule: this.rule.map((g) => g.toGrammar()),
-    });
+    this.rule = data.rule;
+    this.NT = data.NT;
+    this.commit = data.commit;
+    this.callback = data.callback;
+    this.rejecter = data.rejecter;
+    this.rollback = data.rollback;
+    this.traverser = data.traverser;
   }
 
   /**
