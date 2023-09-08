23dc4fff925446e7f8951ed8ad0eedb3d024ec0f
DiscreteTom <discrete_tom@outlook.com>
2023-09-08 20:24:49 +0800

    chore(parser): optimize comments

--- src/parser/ELR/DFA/model.ts
+++ src/parser/ELR/DFA/model.ts
@@ -4,9 +4,15 @@ import { GrammarSet } from "../model";
  * `NT => Grammars`
  */
 export type FirstSets = Map<string, GrammarSet>;
+/**
+ * `NT => Grammars`
+ */
 export type ReadonlyFirstSets = ReadonlyMap<string, GrammarSet>;
 /**
  * `grammar.kind => Grammars`
  */
 export type FollowSets = Map<string, GrammarSet>;
+/**
+ * `grammar.kind => Grammars`
+ */
 export type ReadonlyFollowSets = ReadonlyMap<string, GrammarSet>;
