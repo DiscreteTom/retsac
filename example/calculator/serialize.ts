21f84203e4477d4708d61da8c38cd9a1145ff7f5
DiscreteTom <discrete_tom@outlook.com>
2023-09-09 22:22:03 +0800

    tests: update calculator about serialization

--- example/calculator/serialize.ts
+++
@@ -1,6 +0,0 @@
-import { serializable } from "./core";
-import { writeFileSync } from "fs";
-
-const dfaStr = JSON.stringify(serializable);
-
-writeFileSync("./example/calculator/dfa.json", dfaStr);
