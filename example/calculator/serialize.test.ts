98a18ab7af0ac9d0d3125a423dd8b1cd74be290a
DiscreteTom <discrete_tom@outlook.com>
2023-09-10 11:06:04 +0800

    tests(calculator): update serialize test

--- example/calculator/serialize.test.ts
+++ example/calculator/serialize.test.ts
@@ -1,10 +1,7 @@
 import { readFileSync } from "fs";
 import { serializable } from "./core";
-import { writeFileSync } from "fs";
 
 const dfaStr = JSON.stringify(serializable, null, 2); // 2 spaces for indentation
-
-// writeFileSync("./example/calculator/dfa.json", dfaStr);
 
 test("serialize calculator", () => {
   expect(dfaStr).toBe(readFileSync("./example/calculator/dfa.json", "utf-8"));
