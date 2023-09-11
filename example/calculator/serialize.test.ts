a795b2851d03c2cdd04208e28ffa382f08ab8d1d
DiscreteTom <discrete_tom@outlook.com>
2023-09-11 21:37:22 +0800

    tests: remove unused serialize test since the serialized data will always match the json

--- example/calculator/serialize.test.ts
+++
@@ -1,8 +0,0 @@
-import { readFileSync } from "fs";
-import { serializable } from "./core";
-
-const dfaStr = JSON.stringify(serializable, null, 2); // 2 spaces for indentation
-
-test("serialize calculator", () => {
-  expect(dfaStr).toBe(readFileSync("./example/calculator/dfa.json", "utf-8"));
-});
