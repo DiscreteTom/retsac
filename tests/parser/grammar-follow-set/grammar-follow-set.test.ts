2ec140d6e3e62fc9043711d206b35af329adf915
DiscreteTom <discrete_tom@outlook.com>
2023-09-15 18:52:59 +0800

    tests: fix typo

--- tests/parser/grammar-follow-set/grammar-follow-set.test.ts
+++ tests/parser/grammar-follow-set/grammar-follow-set.test.ts
@@ -3,7 +3,6 @@ import { ELR, Lexer } from "../../../src";
 test("Terminator's follow sets are needed", () => {
   expect(() => {
     new ELR.ParserBuilder()
-      .entry("test")
       .define({
         // when we calculate follow set, the terminator's follow set is needed as well.
         // in the following example, the follow set of `b` is needed, and the value is [`c`]
@@ -11,6 +10,7 @@ test("Terminator's follow sets are needed", () => {
         // then when we check conflicts, we can detect the R-S conflict between `a b` and `a b c`
         test: `a b | a b c`,
       })
+      .entry("test")
       .build(new Lexer.Builder().build(), { checkConflicts: true });
   }).toThrow(`Unresolved R-S conflict`);
 });
