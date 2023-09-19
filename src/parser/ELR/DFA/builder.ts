fd77f0be699fa60a75718346560024f412486766
DiscreteTom <discrete_tom@outlook.com>
2023-09-19 23:12:36 +0800

    chore(parser): update comments

--- src/parser/ELR/DFA/builder.ts
+++ src/parser/ELR/DFA/builder.ts
@@ -143,6 +143,9 @@ export class DFABuilder {
       });
     });
     // the last grammar's follow set should merge with the target NT's follow set
+    // be ware: don't merge the target NT's follow set with the last grammar's follow set
+    // the last grammar's follow set should be a super set of the target NT's follow set, not vice versa
+    // TODO: add tests
     while (true) {
       let changed = false;
 
