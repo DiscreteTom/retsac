b10b1c3725f0988e3e004b2e5bd9edc6804d6c0d
DiscreteTom <discrete_tom@outlook.com>
2023-09-23 22:45:46 +0800

    chore: update utils

--- utils/generate-serialized-grammar-parser.ts
+++ utils/generate-serialized-grammar-parser.ts
@@ -1,5 +1,5 @@
 import { writeFileSync } from "fs";
-import { grammarParserFactory } from "../src/parser/ELR/advanced/utils/grammar-expander";
+import { grammarParserFactory } from "../src/parser/ELR/advanced/utils/grammar-parser-factory";
 import prettier from "prettier";
 import util from "node:util";
 
