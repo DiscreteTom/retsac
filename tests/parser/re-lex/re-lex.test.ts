20b9ee48c2ef2e186fe2719e28ce7a6dc624d737
DiscreteTom <discrete_tom@outlook.com>
2023-09-25 23:11:06 +0800

    tests: add parser/re-lex

---
+++ tests/parser/re-lex/re-lex.test.ts
@@ -0,0 +1,29 @@
+import { ELR, Lexer } from "../../../src";
+
+test("re-lex with expectation", () => {
+  const logger = jest.fn();
+
+  const lexer = new Lexer.Builder()
+    .ignore(Lexer.whitespaces())
+    .define({ num: Lexer.numericLiteral() })
+    .anonymous(...Lexer.exactArray("-", "--"))
+    .build({
+      // debug: true,
+      // logger,
+    });
+
+  const { parser } = new ELR.AdvancedBuilder()
+    .define({ exp: `num ('-' | '--') num` })
+    .entry("exp")
+    .build({
+      lexer,
+      debug: true,
+      logger,
+    });
+
+  // this will try `num '-' num` first, but failed, then re-lex and try `num '--' num`
+  expect(parser.parse("1--1").accept).toBe(true);
+  expect(logger).toHaveBeenCalledWith(
+    '[Re-Lex] Restored input: "--" Trying: ASTNode({ kind: "", start: 1, text: "--", data: undefined, error: undefined })',
+  );
+});
