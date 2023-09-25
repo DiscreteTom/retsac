d7329fcb082ccc42615d713d592afd61c2c642ff
DiscreteTom <discrete_tom@outlook.com>
2023-09-25 23:23:52 +0800

    tests: update comments

--- examples/lexer/lexer-error-handling/lexer-error-handling.ts
+++ examples/lexer/lexer-error-handling/lexer-error-handling.ts
@@ -1,5 +1,4 @@
 import { Lexer } from "../../../src";
-import { Action, numericLiteral, stringLiteral } from "../../../src/lexer";
 
 /**
  * This example shows how to handle errors without stopping the parsing process.
@@ -9,18 +8,28 @@ export const lexer = new Lexer.Builder()
   .define({
     // built-in utils will check unclosed strings and invalid numbers
     // and accept the input with error
-    string: stringLiteral(`"`),
-    number: numericLiteral(),
+    string: Lexer.stringLiteral(`"`),
+    number: Lexer.numericLiteral(),
     // you can customize your own error handling function using `check`
-    identifier: Action.from(/\w+/).check(({ content }) =>
+    identifier: Lexer.Action.from(/\w+/).check(({ content }) =>
       content.match(/\d/)
         ? "identifier should not starts with a number"
         : undefined,
     ),
   })
   // if you are **not working with parser**, you can define a fallback rule
-  // to accept one character at a time
-  .ignore(/./)
+  // to accept one character at a time.
+  // when you are working with parser, lexer's rejection is an important signal
+  // for the parser, so we shouldn't define a fallback rule.
+  .ignore(
+    Lexer.Action.from(/./).then(({ content }) =>
+      // print some logs
+      console.log(`eat "${content}" as a fallback`),
+    ),
+  )
   .build();
 
-// TODO: take & takeUntil
+// if the lexing process is stopped, you can also use `take` and `takeUntil`
+// to eat the input manually.
+// lexer.take(10) // eat 10 characters
+// lexer.takeUntil(/}/) // eat until `}`
