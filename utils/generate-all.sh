115b9a2b2a238107b10da892a8c7ffdb5f5ea735
DiscreteTom <discrete_tom@outlook.com>
2023-09-26 20:28:41 +0800

    chore: use sh to generate all instead of batch

--- utils/generate-all.sh
+++ utils/generate-all.sh
@@ -1,18 +1,18 @@
-:: grammar parser data for advanced parser builder
+# grammar parser data for advanced parser builder
 ts-node utils/generate-serialized-grammar-parser.ts
 
-:: example/parser/advanced-builder
+# example/parser/advanced-builder
 ts-node examples/parser/advanced-builder/mermaid-gen.ts
 ts-node examples/parser/advanced-builder/parser-data-gen.ts
 
-:: example/parser/json
+# example/parser/json
 ts-node examples/parser/json/mermaid-gen.ts
 ts-node examples/parser/json/parser-data-gen.ts
 
-:: example/parser/calculator
+# example/parser/calculator
 ts-node examples/parser/calculator/mermaid-gen.ts
 ts-node examples/parser/calculator/parser-data-gen.ts
 
-:: example/parser/simple-ts-parser
+# example/parser/simple-ts-parser
 ts-node examples/parser/simple-ts-parser/mermaid-gen.ts
 ts-node examples/parser/simple-ts-parser/parser-data-gen.ts
