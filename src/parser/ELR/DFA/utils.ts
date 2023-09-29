7e49736acb0c7eb956affeceb23b8f7b93e94af9
DiscreteTom <discrete_tom@outlook.com>
2023-09-30 00:29:53 +0800

    chore(parser): update comments

--- src/parser/ELR/DFA/utils.ts
+++ src/parser/ELR/DFA/utils.ts
@@ -259,6 +259,14 @@ export function calculateAllStates<
   // convert to mock AST node
   const mockNodes = gs.map((g) => g.mockNode.value);
 
+  // TODO: should we also convert entry-only NTs into ASTNodes?
+  // those entry-only NTs don't appear in grammar rules
+  // but when we enable ignoreEntryFollow, the entry-only NTs
+  // may appear as the first node in parser's buffer,
+  // and the `parser.parse` will throw StateCacheMissError.
+  // if we do convert entry-only NTs into ASTNodes,
+  // the `parser.parse` will just reject the input without throwing StateCacheMissError.
+
   while (true) {
     let changed = false;
     allStates.states.forEach((state) => {
