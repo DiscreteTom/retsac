fe0dd51f980c40a21a2fc3882183c78105c29b76
DiscreteTom <discrete_tom@outlook.com>
2023-09-30 15:58:44 +0800

    chore: optimize generate-all.sh

--- utils/generate-all.sh
+++ utils/generate-all.sh
@@ -1,18 +1,26 @@
+# Usage: . utils/generate-all.sh
+
+# enable echo
+set -x
+
 # grammar parser data for advanced parser builder
-ts-node utils/generate-serialized-grammar-parser.ts
+time ts-node utils/generate-serialized-grammar-parser.ts
 
 # example/parser/advanced-builder
-ts-node examples/parser/advanced-builder/mermaid-gen.ts
-ts-node examples/parser/advanced-builder/parser-data-gen.ts
+time ts-node examples/parser/advanced-builder/mermaid-gen.ts
+time ts-node examples/parser/advanced-builder/parser-data-gen.ts
 
 # example/parser/json
-ts-node examples/parser/json/mermaid-gen.ts
-ts-node examples/parser/json/parser-data-gen.ts
+time ts-node examples/parser/json/mermaid-gen.ts
+time ts-node examples/parser/json/parser-data-gen.ts
 
 # example/parser/calculator
-ts-node examples/parser/calculator/mermaid-gen.ts
-ts-node examples/parser/calculator/parser-data-gen.ts
+time ts-node examples/parser/calculator/mermaid-gen.ts
+time ts-node examples/parser/calculator/parser-data-gen.ts
 
 # example/parser/simple-ts-parser
-ts-node examples/parser/simple-ts-parser/mermaid-gen.ts
-ts-node examples/parser/simple-ts-parser/parser-data-gen.ts
+time ts-node examples/parser/simple-ts-parser/mermaid-gen.ts
+time ts-node examples/parser/simple-ts-parser/parser-data-gen.ts
+
+# disable echo
+set +x
