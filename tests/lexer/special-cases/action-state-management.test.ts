b3652fcf766f1c3d02ff7f5a24abcca27381a0fe
DiscreteTom <discrete_tom@outlook.com>
2023-10-13 20:26:46 +0800

    tests: add lexer action state tests

---
+++ tests/lexer/special-cases/action-state-management.test.ts
@@ -0,0 +1,44 @@
+import { Lexer } from "../../../src";
+
+test("lexer action state", () => {
+  const lexer = new Lexer.Builder()
+    .useState({ value: 1 })
+    .define({ number: /\d+/ }, (a) =>
+      a.then(({ output, input }) => {
+        input.state.value += Number(output.content);
+      }),
+    )
+    .build();
+
+  // ensure initial value
+  expect(lexer.core.state.value).toBe(1);
+
+  // modify the state
+  lexer.lex("123");
+  expect(lexer.core.state.value).toBe(124);
+
+  // reset the value
+  lexer.reset();
+  expect(lexer.core.state.value).toBe(1);
+
+  // clone the lexer
+  const clone = lexer.clone();
+
+  // ensure initial value
+  expect(clone.core.state.value).toBe(1);
+
+  // modify the state
+  clone.lex("123");
+  expect(clone.core.state.value).toBe(124);
+
+  // original lexer should not be affected
+  expect(lexer.core.state.value).toBe(1);
+
+  // clone with state
+  const cloneWithState = clone.clone();
+  expect(cloneWithState.core.state.value).toBe(124);
+
+  // dry clone
+  const dryClone = clone.dryClone();
+  expect(dryClone.core.state.value).toBe(1);
+});
