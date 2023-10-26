e4fe70ceccdbd2b2113374aaefa61475a728bbc6
DiscreteTom <discrete_tom@outlook.com>
2023-10-26 19:23:08 +0800

    feat(lexer): TokenDataBinding, #29

--- src/lexer/model/token.ts
+++ src/lexer/model/token.ts
@@ -1,10 +1,38 @@
-/** The output of a lexer. */
-export type Token<ErrorType, Kinds extends string> = {
-  /** User-defined token kind name. */
+/**
+ * Bind token's kind with its data type.
+ */
+export type TokenDataBinding<Kinds extends string, Data> = {
+  /**
+   * User-defined token kind name.
+   */
   kind: Kinds;
-  /** Text content. */
-  content: string;
-  /** Start position of input string. */
-  start: number;
-  error?: ErrorType;
+  /**
+   * User-defined data stored in this token.
+   */
+  data: Data;
 };
+
+/**
+ * The output of a lexer.
+ */
+// TODO: re-order generic types
+// TODO: helper type to extract token generic parameters
+export type Token<
+  ErrorType,
+  Kinds extends string,
+  Data,
+  DataBindings extends TokenDataBinding<Kinds, Data>,
+> = {
+  /**
+   * Token's text content.
+   */
+  content: string;
+  /**
+   * Index of the first char of this token in the whole input string.
+   */
+  start: number;
+  /**
+   * `undefined` means no error.
+   */
+  error?: ErrorType;
+} & DataBindings;
