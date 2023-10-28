5b434d6769457061082b1f2031b6b65d7357ea38
DiscreteTom <discrete_tom@outlook.com>
2023-10-28 16:48:25 +0800

    chore(lexer): optimize code

--- src/lexer/utils/javascript.ts
+++ src/lexer/utils/javascript.ts
@@ -2,6 +2,19 @@ import type { Action } from "../action";
 import { comment as commonComment } from "./common";
 
 /**
+ * Return an action that matches JavaScript comments (single line and multi line).
+ */
+export function comment<
+  Data = never,
+  ActionState = never,
+  ErrorType = never,
+>(): Action<Data, ActionState, ErrorType> {
+  return commonComment<Data, ActionState, ErrorType>("//").or(
+    commonComment("/*", "*/"),
+  );
+}
+
+/**
  * Evaluate a JavaScript string literal just like `eval`.
  * Caller should make sure the string is well-quoted and well-ended.
  * Interpolation is not supported.
@@ -50,15 +63,5 @@ export function evalString(quoted: string) {
         }
       }
     },
-  );
-}
-
-export function comment<
-  Data = never,
-  ActionState = never,
-  ErrorType = never,
->(): Action<Data, ActionState, ErrorType> {
-  return commonComment<Data, ActionState, ErrorType>("//").or(
-    commonComment("/*", "*/"),
   );
 }
