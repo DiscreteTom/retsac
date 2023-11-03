8c7386f11355cd804fc3b10fe262e0422520c6cc
DiscreteTom <discrete_tom@outlook.com>
2023-11-03 19:48:56 +0800

    tests: add tests for lazy

---
+++ tests/lazy.test.ts
@@ -0,0 +1,22 @@
+import { Lazy } from "../src/lazy";
+
+describe("lazy", () => {
+  test("without initial value", () => {
+    expect(new Lazy(() => "123").raw).toBe(undefined);
+  });
+
+  test("with initial value", () => {
+    expect(new Lazy(() => "123", "123").raw).toBe("123");
+  });
+
+  test("get value", () => {
+    expect(new Lazy(() => "123").value).toBe("123");
+  });
+
+  test("reset value", () => {
+    const lazy = new Lazy(() => "123", "456");
+    lazy.reset();
+    expect(lazy.raw).toBe(undefined);
+    expect(lazy.value).toBe("123");
+  });
+});
