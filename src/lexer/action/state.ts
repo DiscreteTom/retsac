39d4fb9ddd82ab5715ecb8005b61b6f5225f4c38
DiscreteTom <discrete_tom@outlook.com>
2023-12-01 19:24:39 +0800

    chore(lexer): optimize var name

--- src/lexer/action/state.ts
+++ src/lexer/action/state.ts
@@ -2,14 +2,14 @@
  * Clone action state in a proper way.
  */
 export type ActionStateCloner<ActionState> = (
-  ctx: Readonly<ActionState>,
+  state: Readonly<ActionState>,
 ) => ActionState;
 
 /**
- * Clone action state with structured clone(deep clone).
+ * Clone action state with structured clone (deep clone).
  */
 export function defaultActionStateCloner<ActionState>(
-  ctx: Readonly<ActionState>,
+  state: Readonly<ActionState>,
 ): ActionState {
-  return structuredClone(ctx);
+  return structuredClone(state);
 }
