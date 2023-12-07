091f9cead4e390f99a64f0da9a1d639023756932
DiscreteTom <discrete_tom@outlook.com>
2023-12-07 19:01:26 +0800

    chore(lexer): init new stringLiteral

--- src/lexer/utils/string.ts
+++ src/lexer/utils/string.ts
@@ -1,5 +1,5 @@
 import { compose } from "@discretetom/r-compose";
-import { Action } from "../action";
+import { Action, rejectedActionOutput } from "../action";
 
 /**
  * Match a string literal.
@@ -11,7 +11,7 @@ import { Action } from "../action";
  *
  * Set `multiline: true` to allow multiline string literals.
  */
-export function stringLiteral<ActionState = never, ErrorType = never>(
+export function _stringLiteral<ActionState = never, ErrorType = never>(
   /** The open quote. */
   open: string,
   options?: {
@@ -110,4 +110,212 @@ export function stringLiteral<ActionState = never, ErrorType = never>(
 
   // else, not accept unclosed
   return action;
+}
+
+export type EscapeInfo = {
+  /**
+   * The evaluated string value. Errors should be correctly handled.
+   */
+  value: string;
+  /**
+   * The index of the start of the escape starter in the buffer.
+   */
+  start: number;
+  /**
+   * The length of the whole escape sequence, including the escape starter.
+   */
+  length: number;
+  errors: {
+    /**
+     * The index of the whole input text.
+     */
+    start: number;
+    length: number;
+  }[];
+};
+
+export type EscapeHandlerOutput =
+  | { accept: false }
+  | ({ accept: true } & Pick<EscapeInfo, "value" | "length" | "errors">);
+
+export type EscapeHandler = (
+  /**
+   * The whole input text.
+   */
+  buffer: string,
+  /**
+   * The index of the start of the escape starter in the buffer.
+   */
+  start: number,
+) => EscapeHandlerOutput;
+
+// TODO: better name
+export type StringLiteralCondition = (
+  buffer: string,
+  start: number,
+) =>
+  | {
+      accept: true;
+      digested: number;
+    }
+  | { accept: false };
+
+export function stringLiteral<ActionState = never, ErrorType = never>(
+  /**
+   * The open quote.
+   */
+  open: string | StringLiteralCondition,
+  options?: {
+    /**
+     * The close quote.
+     * Equals to the open quote by default.
+     */
+    close?: string | StringLiteralCondition;
+    /**
+     * @default false
+     */
+    multiline?: boolean;
+    /**
+     * @default '\\'
+     */
+    escapeStarter?: string;
+    /**
+     * @default []
+     */
+    escapeHandler?: EscapeHandler[];
+    /**
+     * If `true`, unclosed strings
+     * will also be accepted and marked as `{ unclosed: true }` in `output.data`.
+     * @default true
+     */
+    acceptUnclosed?: boolean;
+  },
+): Action<
+  {
+    kind: never;
+    data: {
+      /**
+       * The evaluated string value. Errors will be correctly handled.
+       */
+      value: string;
+      /**
+       * If `true`, the string literal is unclosed (`\n` or EOF for single line string, and EOF for multiline string).
+       */
+      unclosed: boolean;
+      escapes: EscapeInfo[];
+    };
+  },
+  ActionState,
+  ErrorType
+> {
+  const openMatcher = typeof open === "string" ? string2matcher(open) : open;
+  const closeMatcher =
+    options?.close === undefined
+      ? openMatcher // defaults to the open quote
+      : typeof options.close === "string"
+      ? string2matcher(options.close)
+      : options.close;
+  const escapeStarter = options?.escapeStarter ?? "\\";
+  const multiline = options?.multiline ?? false;
+  const acceptUnclosed = options?.acceptUnclosed ?? true;
+  const escapeHandler = options?.escapeHandler ?? [];
+
+  return Action.exec((input) => {
+    // match open quote
+    const matchOpen = openMatcher(input.buffer, input.start);
+    if (!matchOpen.accept) return rejectedActionOutput;
+
+    const text = input.buffer;
+    const end = input.buffer.length;
+
+    let start = input.start;
+    let pos = input.start + matchOpen.digested; // eat the open quote
+    let value = "";
+    let unclosed = false;
+    const escapes = [] as EscapeInfo[];
+
+    while (true) {
+      // check for unterminated string
+      if (pos >= end) {
+        if (!acceptUnclosed) return rejectedActionOutput;
+
+        value += text.substring(start, pos);
+        unclosed = true;
+        break;
+      }
+
+      // check for close quote
+      const matchClose = closeMatcher(text, pos);
+      if (matchClose.accept) {
+        value += text.substring(start, pos);
+        pos += matchClose.digested; // eat the close quote
+        break;
+      }
+
+      // handle escape
+      if (text.startsWith(escapeStarter, pos)) {
+        let gotEscape = false;
+        for (const handle of escapeHandler) {
+          const res = handle(text, pos);
+          if (res.accept) {
+            escapes.push({
+              start: pos,
+              value: res.value,
+              length: res.length,
+              errors: res.errors,
+            });
+            value += res.value;
+            pos += res.length;
+            start = pos;
+            gotEscape = true;
+            break; // only accept the first accepted handler
+          }
+        }
+
+        // skip `pos++` below since we've already updated `pos`
+        if (gotEscape) continue;
+
+        // else, no escape handler accepted, treat the escape starter as a normal character
+        pos += escapeStarter.length;
+        continue;
+      }
+
+      // handle newline
+      const ch = text.charCodeAt(pos);
+      if (
+        ch === /* CharacterCodes.lineFeed */ 10 ||
+        ch === /* CharacterCodes.carriageReturn */ 13
+      ) {
+        if (multiline) {
+          value += text.substring(start, pos);
+        } else {
+          if (!acceptUnclosed) return rejectedActionOutput;
+
+          value += text.substring(start, pos);
+          unclosed = true;
+          break;
+        }
+      }
+      pos++;
+    }
+
+    return {
+      accept: true,
+      content: input.buffer.slice(input.start, pos),
+      data: {
+        value,
+        unclosed,
+        escapes,
+      },
+      digested: pos - input.start,
+      muted: false,
+    };
+  });
+}
+
+function string2matcher(s: string): StringLiteralCondition {
+  return (buffer, start) =>
+    buffer.startsWith(s, start)
+      ? { accept: true, digested: s.length }
+      : { accept: false };
 }
