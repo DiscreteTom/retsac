5eda2fe3f865109b528ed6b81804767089e16725
DiscreteTom <discrete_tom@outlook.com>
2023-12-16 20:12:12 +0800

    test(lexer): update utils/javascript/numeric

--- tests/lexer/utils/javascript/numeric.test.ts
+++ tests/lexer/utils/javascript/numeric.test.ts
@@ -465,3 +465,603 @@ describe("integer literals", () => {
     });
   });
 });
+
+describe("numericLiteral", () => {
+  function expectAccept(
+    lexer: Lexer.Lexer<
+      {
+        kind: "number";
+        data: Lexer.javascript.NumericLiteralData;
+      },
+      never,
+      never
+    >,
+    input: string,
+    overrides: Partial<
+      Omit<NonNullable<ReturnType<typeof lexer.lex>>, "data">
+    > & {
+      data: Partial<NonNullable<ReturnType<typeof lexer.lex>>["data"]> &
+        Pick<NonNullable<ReturnType<typeof lexer.lex>>["data"], "integer">;
+    },
+  ) {
+    const token = lexer.reset().lex(input)!;
+    expect(token.content).toBe(overrides?.content ?? input);
+    expect(token.kind).toBe("number");
+    expect(token.error).toBe(undefined);
+    expect(token.data.integer).toEqual(overrides.data.integer);
+    expect(token.data.fraction).toEqual(overrides.data.fraction ?? undefined);
+    expect(token.data.exponent).toEqual(overrides.data.exponent ?? undefined);
+    expect(token.data.suffix).toBe(overrides.data.suffix ?? "");
+    expect(token.data.separators).toEqual(overrides.data.separators ?? []);
+    expect(token.data.emptyExponent).toBe(
+      overrides.data.emptyExponent ?? false,
+    );
+    expect(token.data.leadingZero).toBe(overrides.data.leadingZero ?? false);
+    expect(token.data.bigIntWithFraction).toBe(
+      overrides.data.bigIntWithFraction ?? false,
+    );
+    expect(token.data.bigIntWithExponent).toBe(
+      overrides.data.bigIntWithExponent ?? false,
+    );
+    expect(token.data.missingBoundary).toBe(
+      overrides.data.missingBoundary ?? false,
+    );
+    expect(token.data.invalidSeparatorIndexes).toEqual(
+      overrides.data.invalidSeparatorIndexes ?? [],
+    );
+    expect(token.data.consecutiveSeparatorIndexes).toEqual(
+      overrides.data.consecutiveSeparatorIndexes ?? [],
+    );
+  }
+
+  function expectReject(
+    lexer: Lexer.Lexer<
+      {
+        kind: "number";
+        data: Lexer.javascript.NumericLiteralData;
+      },
+      never,
+      never
+    >,
+    input: string,
+  ) {
+    expect(lexer.reset().lex(input)).toBe(null);
+  }
+
+  describe("default", () => {
+    const lexer = new Lexer.Builder()
+      .define({ number: Lexer.javascript.numericLiteral() })
+      .build();
+
+    test("not numeric literal", () => {
+      expectReject(lexer, "abc");
+    });
+
+    test("integer only", () => {
+      expectAccept(lexer, "123", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+        },
+      });
+    });
+
+    test("integer with separators", () => {
+      expectAccept(lexer, "1_2_3", {
+        data: {
+          integer: {
+            digested: 5,
+            value: "123",
+          },
+          separators: [
+            { index: 1, content: "_" },
+            { index: 3, content: "_" },
+          ],
+        },
+      });
+    });
+
+    test("integer with suffix", () => {
+      expectAccept(lexer, "123n", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          suffix: "n",
+        },
+      });
+    });
+
+    test("integer and fraction", () => {
+      expectAccept(lexer, "123.456", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          fraction: {
+            point: {
+              index: 3,
+              content: ".",
+            },
+            index: 4,
+            digested: 3,
+            value: "456",
+          },
+        },
+      });
+    });
+
+    test("fraction only", () => {
+      expectAccept(lexer, ".456", {
+        data: {
+          integer: {
+            digested: 0,
+            value: "",
+          },
+          fraction: {
+            point: {
+              index: 0,
+              content: ".",
+            },
+            index: 1,
+            digested: 3,
+            value: "456",
+          },
+        },
+      });
+    });
+
+    test("integer and fraction with separator", () => {
+      expectAccept(lexer, "1_2_3.4_5_6", {
+        data: {
+          integer: {
+            digested: 5,
+            value: "123",
+          },
+          fraction: {
+            point: {
+              index: 5,
+              content: ".",
+            },
+            index: 6,
+            digested: 5,
+            value: "456",
+          },
+          separators: [
+            { index: 1, content: "_" },
+            { index: 3, content: "_" },
+            { index: 7, content: "_" },
+            { index: 9, content: "_" },
+          ],
+        },
+      });
+    });
+
+    test("fraction with separator", () => {
+      expectAccept(lexer, ".4_5_6", {
+        data: {
+          integer: {
+            digested: 0,
+            value: "",
+          },
+          fraction: {
+            point: {
+              index: 0,
+              content: ".",
+            },
+            index: 1,
+            digested: 5,
+            value: "456",
+          },
+          separators: [
+            { index: 2, content: "_" },
+            { index: 4, content: "_" },
+          ],
+        },
+      });
+    });
+
+    test("fraction with suffix", () => {
+      expectAccept(lexer, ".456n", {
+        data: {
+          integer: {
+            digested: 0,
+            value: "",
+          },
+          fraction: {
+            point: {
+              index: 0,
+              content: ".",
+            },
+            index: 1,
+            digested: 3,
+            value: "456",
+          },
+          suffix: "n",
+          bigIntWithFraction: true,
+        },
+      });
+    });
+
+    test("integer and exponent", () => {
+      expectAccept(lexer, "123e456", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "e",
+            },
+            index: 4,
+            digested: 3,
+            value: "456",
+          },
+        },
+      });
+      expectAccept(lexer, "123E456", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "E",
+            },
+            index: 4,
+            digested: 3,
+            value: "456",
+          },
+        },
+      });
+    });
+
+    test("fraction and exponent", () => {
+      expectAccept(lexer, "123.456e789", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          fraction: {
+            point: {
+              index: 3,
+              content: ".",
+            },
+            index: 4,
+            digested: 3,
+            value: "456",
+          },
+          exponent: {
+            indicator: {
+              index: 7,
+              content: "e",
+            },
+            index: 8,
+            digested: 3,
+            value: "789",
+          },
+        },
+      });
+    });
+
+    test("exponent with sign", () => {
+      expectAccept(lexer, "123e+456", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "e+",
+            },
+            index: 5,
+            digested: 3,
+            value: "456",
+          },
+        },
+      });
+      expectAccept(lexer, "123e-456", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "e-",
+            },
+            index: 5,
+            digested: 3,
+            value: "456",
+          },
+        },
+      });
+    });
+
+    test("exponent with suffix", () => {
+      expectAccept(lexer, "123e456n", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "e",
+            },
+            index: 4,
+            digested: 3,
+            value: "456",
+          },
+          suffix: "n",
+          bigIntWithExponent: true,
+        },
+      });
+    });
+
+    test("exponent with separator", () => {
+      expectAccept(lexer, "123e4_5_6", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "e",
+            },
+            index: 4,
+            digested: 5,
+            value: "456",
+          },
+          separators: [
+            { index: 5, content: "_" },
+            { index: 7, content: "_" },
+          ],
+        },
+      });
+    });
+
+    test("empty exponent", () => {
+      expectAccept(lexer, "123e", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "e",
+            },
+            index: 4,
+            digested: 0,
+            value: "",
+          },
+          emptyExponent: true,
+        },
+      });
+    });
+
+    test("leading zero", () => {
+      expectAccept(lexer, "0123", {
+        data: {
+          integer: {
+            digested: 4,
+            value: "0123",
+          },
+          leadingZero: true,
+        },
+      });
+    });
+
+    test("big int with fraction", () => {
+      expectAccept(lexer, "123.456n", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          fraction: {
+            point: {
+              index: 3,
+              content: ".",
+            },
+            index: 4,
+            digested: 3,
+            value: "456",
+          },
+          suffix: "n",
+          bigIntWithFraction: true,
+        },
+      });
+    });
+
+    test("big int with exponent", () => {
+      expectAccept(lexer, "123e456n", {
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          exponent: {
+            indicator: {
+              index: 3,
+              content: "e",
+            },
+            index: 4,
+            digested: 3,
+            value: "456",
+          },
+          suffix: "n",
+          bigIntWithExponent: true,
+        },
+      });
+    });
+
+    test("missing boundary", () => {
+      expectAccept(lexer, "123abc", {
+        content: "123",
+        data: {
+          integer: {
+            digested: 3,
+            value: "123",
+          },
+          missingBoundary: true,
+        },
+      });
+    });
+
+    describe("invalid separators", () => {
+      test("starts with 0_", () => {
+        expectAccept(lexer, "0_123", {
+          data: {
+            integer: {
+              digested: 5,
+              value: "0123",
+            },
+            separators: [{ index: 1, content: "_" }],
+            invalidSeparatorIndexes: [1],
+            leadingZero: true,
+          },
+        });
+      });
+
+      test("separator should not be at start or end of each part", () => {
+        expectAccept(lexer, "_1_._1_e_1_n", {
+          data: {
+            integer: {
+              digested: 3,
+              value: "1",
+            },
+            fraction: {
+              point: {
+                index: 3,
+                content: ".",
+              },
+              index: 4,
+              digested: 3,
+              value: "1",
+            },
+            exponent: {
+              indicator: {
+                index: 7,
+                content: "e",
+              },
+              index: 8,
+              digested: 3,
+              value: "1",
+            },
+            suffix: "n",
+            separators: [
+              { index: 0, content: "_" },
+              { index: 2, content: "_" },
+              { index: 4, content: "_" },
+              { index: 6, content: "_" },
+              { index: 8, content: "_" },
+              { index: 10, content: "_" },
+            ],
+            invalidSeparatorIndexes: [0, 2, 4, 6, 8, 10],
+            bigIntWithFraction: true,
+            bigIntWithExponent: true,
+          },
+        });
+      });
+    });
+
+    test("consecutive separators", () => {
+      expectAccept(lexer, "1__2__3", {
+        data: {
+          integer: {
+            digested: 7,
+            value: "123",
+          },
+          separators: [
+            { index: 1, content: "_" },
+            { index: 2, content: "_" },
+            { index: 4, content: "_" },
+            { index: 5, content: "_" },
+          ],
+          consecutiveSeparatorIndexes: [2, 5],
+        },
+      });
+    });
+  });
+
+  describe("reject invalid", () => {
+    describe("require boundary", () => {
+      const lexer = new Lexer.Builder()
+        .define({
+          number: Lexer.javascript.numericLiteral({ acceptInvalid: false }),
+        })
+        .build();
+
+      test("missing boundary", () => {
+        expectReject(lexer, "123abc");
+      });
+
+      test("empty exponent", () => {
+        expectReject(lexer, "123e");
+      });
+
+      test("leading zero", () => {
+        expectReject(lexer, "0123");
+      });
+
+      test("big int with fraction", () => {
+        expectReject(lexer, "123.456n");
+      });
+
+      test("big int with exponent", () => {
+        expectReject(lexer, "123e456n");
+      });
+
+      test("invalid separators", () => {
+        expectReject(lexer, "0_123");
+        expectReject(lexer, "_1_._1_e_1_n");
+      });
+
+      test("consecutive separators", () => {
+        expectReject(lexer, "1__2__3");
+      });
+    });
+
+    describe("don't require boundary", () => {
+      const lexer = new Lexer.Builder()
+        .define({
+          number: Lexer.javascript.numericLiteral({
+            acceptInvalid: false,
+            boundary: false,
+          }),
+        })
+        .build();
+
+      test("missing boundary", () => {
+        expectAccept(lexer, "123abc", {
+          content: "123",
+          data: {
+            integer: {
+              digested: 3,
+              value: "123",
+            },
+            missingBoundary: true,
+          },
+        });
+      });
+    });
+  });
+});
