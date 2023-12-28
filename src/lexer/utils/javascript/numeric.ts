694afff1a77c64171ede1e6d21a203401d64992f
DiscreteTom <discrete_tom@outlook.com>
2023-12-28 21:43:59 +0800

    chore(lexer): refactor js numeric literal

--- src/lexer/utils/javascript/numeric.ts
+++ src/lexer/utils/javascript/numeric.ts
@@ -132,30 +132,36 @@ export function hexIntegerLiteral<
   }).data(integerLiteralDataMapper);
 }
 
-export type NumericLiteralOptions = {
-  /**
-   * If `true`, the numeric literal must have a boundary at the end (non inclusive).
-   * @default true
-   */
-  boundary?: boolean;
-  /**
-   * If `true`, common invalid numeric literals will also be accepted and marked in `output.data` with `{ invalid: true }`.
-   * @default true
-   */
-  acceptInvalid?: boolean;
-};
-
 export type NumericLiteralData = {
-  emptyExponent: boolean;
-  leadingZero: boolean;
-  bigIntWithFraction: boolean;
-  bigIntWithExponent: boolean;
-  missingBoundary: boolean;
-  invalidSeparatorIndexes: number[];
   /**
-   * The index of the whole input string where the consecutive separator is located.
+   * The value of the numeric literal.
+   * This will try to be parsed even if the numeric literal is invalid.
    */
-  consecutiveSeparatorIndexes: number[];
+  value: number | bigint;
+  /**
+   * `undefined` if the numeric literal valid.
+   */
+  invalid?: {
+    /**
+     * If `true`, the exponent identifier is provided but the exponent content is empty.
+     */
+    emptyExponent: boolean;
+    leadingZero: boolean;
+    bigIntWithFraction: boolean;
+    bigIntWithExponent: boolean;
+    /**
+     * If `true`, there is no boundary between the numeric literal and the following identifier.
+     */
+    missingBoundary: boolean;
+    /**
+     * The index of the whole input string where the invalid separator is located.
+     */
+    invalidSeparatorIndexes: number[];
+    /**
+     * The index of the whole input string where the consecutive separator is located.
+     */
+    consecutiveSeparatorIndexes: number[];
+  };
 } & CommonNumericLiteralData;
 
 export function numericLiteralDataMapper<ActionState, ErrorType>({
@@ -166,14 +172,39 @@ export function numericLiteralDataMapper<ActionState, ErrorType>({
   ActionState,
   ErrorType
 >): NumericLiteralData {
-  const invalidSeparatorIndexes = [] as number[];
+  const rawInvalid: NonNullable<NumericLiteralData["invalid"]> = {
+    emptyExponent:
+      output.data.exponent !== undefined &&
+      output.data.exponent.body.length === 0,
+    leadingZero:
+      output.data.integer.body.length > 1 &&
+      output.data.integer.body.startsWith("0"),
+    bigIntWithFraction:
+      output.data.suffix === "n" && output.data.fraction !== undefined,
+    bigIntWithExponent:
+      output.data.suffix === "n" && output.data.exponent !== undefined,
+    missingBoundary:
+      input.buffer.length > input.start + output.digested &&
+      // TODO: support unicode
+      // see https://github.com/microsoft/TypeScript/blob/efc9c065a2caa52c5bebd08d730eed508075a78a/src/compiler/scanner.ts#L957
+      input.buffer[input.start + output.digested].match(/[a-zA-Z_$]/) !== null,
+    invalidSeparatorIndexes: [], // will be filled later
+    consecutiveSeparatorIndexes: output.data.separators
+      .map((s, i, arr) => {
+        if (i === 0) return -1;
+        if (arr[i - 1].index + arr[i - 1].content.length === s.index)
+          return s.index;
+        return -1;
+      })
+      .filter((i) => i !== -1),
+  };
 
   // `0_` is invalid
   if (
-    output.data.integer.value.startsWith("0") &&
+    output.data.integer.body.startsWith("0") &&
     output.data.separators[0]?.index === input.start + 1
   ) {
-    invalidSeparatorIndexes.push(output.data.separators[0].index);
+    rawInvalid.invalidSeparatorIndexes.push(output.data.separators[0].index);
   }
 
   // separator should NOT be at start or end of each part
@@ -197,69 +228,46 @@ export function numericLiteralDataMapper<ActionState, ErrorType>({
         s.index + s.content.length ===
           output.data.exponent.index + output.data.exponent.digested)
     ) {
-      invalidSeparatorIndexes.push(s.index);
+      rawInvalid.invalidSeparatorIndexes.push(s.index);
     }
   });
 
   return {
-    emptyExponent:
-      output.data.exponent !== undefined &&
-      output.data.exponent.value.length === 0,
-    leadingZero:
-      output.data.integer.value.length > 1 &&
-      output.data.integer.value.startsWith("0"),
-    bigIntWithFraction:
-      output.data.suffix === "n" && output.data.fraction !== undefined,
-    bigIntWithExponent:
-      output.data.suffix === "n" && output.data.exponent !== undefined,
-    missingBoundary:
-      input.buffer.length > input.start + output.digested &&
-      // TODO: support unicode
-      // see https://github.com/microsoft/TypeScript/blob/efc9c065a2caa52c5bebd08d730eed508075a78a/src/compiler/scanner.ts#L957
-      input.buffer[input.start + output.digested].match(/[a-zA-Z_$]/) !== null,
-    invalidSeparatorIndexes,
-    consecutiveSeparatorIndexes: output.data.separators
-      .map((s, i, arr) => {
-        if (i === 0) return -1;
-        if (arr[i - 1].index + arr[i - 1].content.length === s.index)
-          return s.index;
-        return -1;
-      })
-      .filter((i) => i !== -1),
+    value: tryOrDefault(
+      () =>
+        output.data.suffix === "n"
+          ? BigInt(output.data.integer.body) // only integer part is allowed
+          : Number(
+              output.data.integer.body +
+                output.data.fraction?.point.content +
+                output.data.fraction?.body +
+                output.data.exponent?.indicator.content +
+                output.data.exponent?.body,
+            ),
+      0,
+    ),
+    invalid:
+      rawInvalid.emptyExponent ||
+      rawInvalid.leadingZero ||
+      rawInvalid.bigIntWithFraction ||
+      rawInvalid.bigIntWithExponent ||
+      rawInvalid.missingBoundary ||
+      rawInvalid.invalidSeparatorIndexes.length > 0 ||
+      rawInvalid.consecutiveSeparatorIndexes.length > 0
+        ? rawInvalid
+        : undefined,
     ...output.data,
   };
 }
-
-export function numericLiteralRejecterFactory<ActionState, ErrorType>(
-  requireBoundary: boolean,
-) {
-  return function ({
-    output,
-  }: AcceptedActionDecoratorContext<
-    { kind: never; data: NumericLiteralData },
-    ActionState,
-    ErrorType
-  >): boolean {
-    return (
-      output.data.emptyExponent ||
-      output.data.leadingZero ||
-      output.data.bigIntWithFraction ||
-      output.data.bigIntWithExponent ||
-      (requireBoundary && output.data.missingBoundary) ||
-      output.data.invalidSeparatorIndexes.length > 0 ||
-      output.data.consecutiveSeparatorIndexes.length > 0
-    );
-  };
-}
-
 /**
- * JavaScript's numeric literal. E.g. `0.123_456e-789n`.
+ * JavaScript's numeric literal, including BigInt. E.g. `-0.123_456e-789n`.
  *
- * Integer literal is not included.
+ * Integer literal is NOT included (e.g. `0b101`).
  */
-export function numericLiteral<ActionState = never, ErrorType = never>(
-  options?: NumericLiteralOptions,
-): Action<
+export function numericLiteral<
+  ActionState = never,
+  ErrorType = never,
+>(): Action<
   {
     kind: never;
     data: NumericLiteralData;
@@ -267,15 +275,11 @@ export function numericLiteral<ActionState = never, ErrorType = never>(
   ActionState,
   ErrorType
 > {
-  const action = commonNumericLiteral<ActionState, ErrorType>({
+  return commonNumericLiteral<ActionState, ErrorType>({
     prefix: /(?:[+-]\s*)?/,
     decimalPoint: ".",
     exponentIndicator: /[eE](?:[+-])?/,
     separator: "_",
     suffix: "n",
   }).data(numericLiteralDataMapper);
-
-  return options?.acceptInvalid ?? true
-    ? action
-    : action.reject(numericLiteralRejecterFactory(options?.boundary ?? true));
 }
