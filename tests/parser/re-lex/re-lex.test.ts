5a6a6b2b130bfa1bd56da3912b52e7382f9e6ad8
DiscreteTom <discrete_tom@outlook.com>
2023-12-30 18:49:59 +0800

    tests(parser): update re-lex

--- tests/parser/re-lex/re-lex.test.ts
+++ tests/parser/re-lex/re-lex.test.ts
@@ -15,20 +15,20 @@ test("re-lex with expectation", () => {
 
   const { parser } = new ELR.AdvancedBuilder()
     .lexer(lexer)
-    .define({ exp: `num ('-' | '--') num` })
+    .define({ exp: `num ('--' | '-' num)` })
     .build({
       entry: "exp",
       debug: true,
       logger,
     });
 
-  // this will try `num '-' num` first, but failed, then re-lex and try `num '--' num`
+  // this will try `num '--'` first, but failed, then re-lex and try `num '-' num`
   expect(parser.parse("1--1").accept).toBe(true);
   expect(printer).toHaveBeenCalledWith(
-    '[Parser] try lex: store other possibilities for re-lex:\n<anonymous>: "--"',
+    '[Parser] try lex: store other possibilities for re-lex:\n<anonymous>: "-"',
   );
   expect(printer).toHaveBeenCalledWith(
-    '[Parser] re-lex, restored: "--", trying: ASTNode({ kind: "", start: 1, text: "--", data: undefined, error: undefined })',
+    '[Parser] re-lex, restored: "--", trying: ASTNode({ kind: "", start: 1, text: "-", data: undefined, error: undefined })',
   );
 });
 
@@ -44,7 +44,7 @@ test("disable re-lex", () => {
 
   const { parser } = new ELR.AdvancedBuilder()
     .lexer(lexer)
-    .define({ exp: `num ('-' | '--') num` })
+    .define({ exp: `num ('--' | '-' num)` })
     .build({
       entry: "exp",
       reLex: false,
