150fd7ef2a848b581916e350c0536aeacef7c53f
DiscreteTom <discrete_tom@outlook.com>
2024-01-04 21:45:45 +0800

    feat(parser)!: rewrite traverser

--- src/parser/ast.ts
+++ src/parser/ast.ts
@@ -5,9 +5,8 @@ import {
   type ASTNodeSelector,
   type ASTNodeFirstMatchSelector,
 } from "./selector";
-import { InvalidTraverseError } from "./error";
-import type { Traverser } from "./traverser";
-import { defaultTraverser } from "./traverser";
+import type { NTNodeTraverser } from "./traverser";
+import { defaultNTNodeTraverser } from "./traverser";
 import { anonymousKindPlaceholder } from "../anonymous";
 import type { StringOrLiteral } from "../helper";
 
@@ -52,14 +51,14 @@ export abstract class ASTNode<
   TokenType extends GeneralToken,
 > {
   /**
-   * T's or NT's kind name.
-   */
-  readonly kind: Kind;
-  /**
    * By default, this is the same as the kind name.
    * You can rename nodes in your grammar rules.
    */
   name: string;
+  /**
+   * T's or NT's kind name.
+   */
+  readonly kind: Kind;
   /**
    * Start position of the whole input string.
    * Same as the first token's start position.
@@ -68,11 +67,11 @@ export abstract class ASTNode<
   /**
    * @default undefined
    */
-  data?: ASTData;
+  data: ASTData | undefined;
   /**
    * @default undefined
    */
-  error?: ErrorType;
+  error: ErrorType | undefined;
 
   // should only be used by subclasses
   protected constructor(
@@ -90,6 +89,22 @@ export abstract class ASTNode<
     this.data = p.data;
     this.error = p.error;
   }
+
+  /**
+   * A type guard for checking the kind of this node.
+   */
+  is<TargetKind extends Kind>(
+    kind: TargetKind,
+  ): this is ASTNode<TargetKind, NTs, ASTData, ErrorType, TokenType> {
+    // TODO: make the return type more accurate
+    return this.kind === kind;
+  }
+
+  /**
+   * Use the traverser to calculate data and return the data.
+   */
+  // TODO: add a parameter as the traverse context
+  abstract traverse(): ASTData | undefined;
 
   /**
    * Return a tree-structured string.
@@ -114,7 +129,7 @@ export abstract class ASTNode<
   }
 
   /**
-   * `toTreeString` with full options.
+   * {@link ASTNode.toTreeString} with full options.
    */
   abstract _toTreeString(options: {
     /**
@@ -140,15 +155,6 @@ export abstract class ASTNode<
    * Return an ASTObj for serialization.
    */
   abstract toJSON(): ASTObj;
-
-  /**
-   * A type guard for checking the kind of this node.
-   */
-  is<TargetKind extends Kind>(
-    kind: TargetKind,
-  ): this is ASTNode<TargetKind, NTs, ASTData, ErrorType, TokenType> {
-    return this.kind === kind;
-  }
 }
 
 export class NTNode<
@@ -178,22 +184,21 @@ export class NTNode<
    * Select children nodes by the name.
    */
   readonly $$: ASTNodeChildrenSelector<NTs, ASTData, ErrorType, TokenType>;
-
   /**
    * `traverser` shouldn't be exposed
    * because we want users to use `traverse` instead of `traverser` directly.
-   * Make this private to prevent users from using it by mistake.
-   *
-   * Only NT node has traverser, since T node is created by token.
+   * Make this non-public to prevent users from using it by mistake.
    */
-  private traverser: Traverser<Kind, ASTData, ErrorType, TokenType>;
+  private traverser: NTNodeTraverser<Kind, NTs, ASTData, ErrorType, TokenType>;
 
   constructor(
     p: Pick<
       NTNode<Kind, NTs, ASTData, ErrorType, TokenType>,
       "kind" | "start" | "data" | "error" | "children"
     > & {
-      traverser?: Traverser<Kind, ASTData, ErrorType, TokenType>;
+      traverser:
+        | NTNodeTraverser<Kind, NTs, ASTData, ErrorType, TokenType>
+        | undefined;
       firstMatchSelector: ASTNodeFirstMatchSelector<
         NTs,
         ASTData,
@@ -204,8 +209,8 @@ export class NTNode<
     },
   ) {
     super(p);
+
     this.children = p.children;
-    this.traverser = p.traverser ?? defaultTraverser;
     this.$ = <
       TargetKind extends StringOrLiteral<NTs | ExtractKinds<TokenType>>,
     >(
@@ -216,10 +221,22 @@ export class NTNode<
     >(
       name: TargetKind,
     ) => p.selector(name, this.children);
+    this.traverser = p.traverser ?? defaultNTNodeTraverser;
+  }
+
+  traverse(): ASTData | undefined {
+    const res = this.traverser(this);
+    this.data =
+      res ??
+      (res === null
+        ? (null as ASTData)
+        : // undefined or void
+          undefined);
+    return this.data;
   }
 
   toString(): string {
-    return `ASTNode(${JSON.stringify({
+    return `NTNode(${JSON.stringify({
       name: this.name,
       kind: this.kind,
       start: this.start,
@@ -227,21 +244,6 @@ export class NTNode<
       // data and error might not able to be serialized
     })})`;
   }
-
-  // _toTreeString(indent: string): string {
-  //   const kind = this.kind === "" ? anonymousKindPlaceholder : this.kind;
-  //   let res = `${indent}${
-  //     this.kind === this.name ? kind : `${kind}@${this.name}`
-  //   }: `;
-  //   if (this.text) res += JSON.stringify(this.text); // quote the text
-  //   res += "\n";
-  //   this.children?.forEach((c) => {
-  //     res += c.toTreeString({
-  //       indent: indent + "  ",
-  //     });
-  //   });
-  //   return res;
-  // }
 
   _toTreeString(options: {
     initial: string;
@@ -272,26 +274,6 @@ export class NTNode<
       children: this.children.map((c) => c.toJSON()),
     };
   }
-
-  /**
-   * Use the traverser to calculate data and return the data.
-   *
-   * @throws {InvalidTraverseError} if the node is a leaf node (no children).
-   */
-  // TODO: add a parameter as the traverse context
-  traverse(): ASTData | undefined {
-    if (this.children === undefined) throw new InvalidTraverseError(this);
-    const res = this.traverser(
-      this as Parameters<Traverser<Kind, ASTData, ErrorType, TokenType>>[0], // children is not undefined
-    );
-    this.data =
-      res ??
-      (res === null
-        ? (null as ASTData)
-        : // undefined or void
-          undefined);
-    return this.data;
-  }
 }
 
 export class TNode<
@@ -306,6 +288,7 @@ export class TNode<
    * This is not readonly because it will be set by parent node.
    */
   parent?: NTNode<NTs, NTs, ASTData, ErrorType, TokenType>;
+  // TODO: remove token, #38
   token: TokenType & { kind: Kind };
 
   /**
@@ -318,12 +301,12 @@ export class TNode<
   constructor(
     p: Pick<
       TNode<Kind, NTs, ASTData, ErrorType, TokenType>,
-      "kind" | "start" | "data" | "error" | "parent" | "token"
+      "kind" | "start" | "token"
     >,
   ) {
-    super(p);
-    this.parent = p.parent;
+    super({ ...p, data: undefined, error: undefined });
     this.token = p.token;
+    // parent is set later by parent node
   }
 
   static from<
@@ -345,8 +328,15 @@ export class TNode<
     });
   }
 
+  traverse(): ASTData | undefined {
+    // for T nodes, data should be set by the user
+    // and the traverse function won't calculate data
+    // just return the data
+    return this.data;
+  }
+
   toString(): string {
-    return `ASTNode(${JSON.stringify({
+    return `TNode(${JSON.stringify({
       name: this.name,
       kind: this.kind,
       start: this.start,
