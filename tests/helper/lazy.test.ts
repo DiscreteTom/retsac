19eea77d6210fd942f665d1c2174f34e69d0a3c8
DiscreteTom <discrete_tom@outlook.com>
2024-01-28 08:34:36 +0800

    tests: test set value for lazy

--- tests/helper/lazy.test.ts
+++ tests/helper/lazy.test.ts
@@ -13,6 +13,16 @@ describe("lazy", () => {
     expect(new Lazy(() => "123").value).toBe("123");
   });
 
+  test("set value", () => {
+    const lazy = new Lazy(() => "123");
+    lazy.value = "456";
+    expect(lazy.raw).toBe("456");
+    expect(lazy.value).toBe("456");
+    lazy.value = undefined;
+    expect(lazy.raw).toBe(undefined);
+    expect(lazy.value).toBe("123");
+  });
+
   test("reset value", () => {
     const lazy = new Lazy(() => "123", "456");
     lazy.reset();
